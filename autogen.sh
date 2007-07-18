47cdf8b969bd86936e7422c744fb44c450278daa
Michael Hanselmann <hansmi@google.com>
2007-07-18 08:38:39 +0000

    Add autogen.bash, move some files to config/

---
+++ autogen.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if test ! -f configure.ac ; then
+  echo "You must execute this script from the top level directory."
+  exit 1
+fi
+
+set -e
+
+rm -rf config.cache autom4te.cache
+mkdir -p config
+
+aclocal-1.9
+autoconf
+automake-1.9 --add-missing
+
+rm -rf autom4te.cache
