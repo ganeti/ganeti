2310354434ca0a5a3fb59b3885d033afcae2b9d2
Michael Hanselmann <hansmi@google.com>
2007-11-21 16:56:49 +0000

    Implement disk templates for burnin from QA.
    Reviewed-by: iustinp

--- qa/qa_cluster.py
+++ qa/qa_cluster.py
@@ -117,26 +117,30 @@ def TestClusterBurnin():
   """Burnin"""
   master = qa_config.GetMasterNode()
 
+  disk_template = (qa_config.get('options', {}).
+                   get('burnin-disk-template', 'remote_raid1'))
+
   # Get as many instances as we need
   instances = []
   try:
-    num = qa_config.get('options', {}).get('burnin-instances', 1)
-    for _ in xrange(0, num):
-      instances.append(qa_config.AcquireInstance())
-  except qa_error.OutOfInstancesError:
-    print "Not enough instances, continuing anyway."
+    try:
+      num = qa_config.get('options', {}).get('burnin-instances', 1)
+      for _ in xrange(0, num):
+        instances.append(qa_config.AcquireInstance())
+    except qa_error.OutOfInstancesError:
+      print "Not enough instances, continuing anyway."
 
-  if len(instances) < 1:
-    raise qa_error.Error("Burnin needs at least one instance")
+    if len(instances) < 1:
+      raise qa_error.Error("Burnin needs at least one instance")
 
-  # Run burnin
-  try:
     script = qa_utils.UploadFile(master['primary'], '../tools/burnin')
     try:
+      # Run burnin
       cmd = [script,
              '--os=%s' % qa_config.get('os'),
              '--os-size=%s' % qa_config.get('os-size'),
-             '--swap-size=%s' % qa_config.get('swap-size')]
+             '--swap-size=%s' % qa_config.get('swap-size'),
+             '--disk-template=%s' % disk_template]
       cmd += [inst['name'] for inst in instances]
       AssertEqual(StartSSH(master['primary'],
                            utils.ShellQuoteArgs(cmd)).wait(), 0)
