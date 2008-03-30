6da57fdd413c399a313ae971a1b4b26857ab40bd
Iustin Pop <iustin@google.com>
2008-03-30 14:55:45 +0000

    Change the order of config updates in some LUs
    In the start and stop instance LUs, the configuration update is done
    right at the end. This means that if, for example, the instance shutdown
    succeeds, but the drive deactivation fails, the next run of the watcher
    will start the instance again, as it's still marked in running mode.
    
    This patch changes these two LUs so that first the update the
    configuration to the desired state, and only then we proceed to update
    the config. This ensures that the state saved is the desired state.
    
    Because the config might be updated even though the LU failed, this
    patch also modifies the mcpu.Processor.ExecOpCode method to run the
    RunConfigUpdate hook in a finally: phase while the lu.Exec is done in
    its try phase. This ensures that config update hooks (tries to) run at
    all times when the config is updated.
    
    Reviewed-by: schreiberal

--- lib/cmdlib.py
+++ lib/cmdlib.py
@@ -2062,6 +2062,8 @@ class LUStartupInstance(LogicalUnit):
     force = self.op.force
     extra_args = getattr(self.op, "extra_args", "")
 
+    self.cfg.MarkInstanceUp(instance.name)
+
     node_current = instance.primary_node
 
     _StartInstanceDisks(self.cfg, instance, force)
@@ -2069,8 +2071,6 @@ class LUStartupInstance(LogicalUnit):
     if not rpc.call_instance_start(node_current, instance, extra_args):
       _ShutdownInstanceDisks(instance, self.cfg)
       raise errors.OpExecError("Could not start instance")
-
-    self.cfg.MarkInstanceUp(instance.name)
 
 
 class LURebootInstance(LogicalUnit):
@@ -2187,10 +2187,10 @@ class LUShutdownInstance(LogicalUnit):
     """
     instance = self.instance
     node_current = instance.primary_node
+    self.cfg.MarkInstanceDown(instance.name)
     if not rpc.call_instance_shutdown(node_current, instance):
       logger.Error("could not shutdown instance")
 
-    self.cfg.MarkInstanceDown(instance.name)
     _ShutdownInstanceDisks(instance, self.cfg)
 
 
