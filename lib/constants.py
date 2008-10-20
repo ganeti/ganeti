a5b67f752fc582934e4c1b84f998f34f7191428d
Guido Trotter <ultrotter@google.com>
2008-10-20 09:45:39 +0000

    Update protocol version
    r1808 changes an OS object field name from api_version to api_versions
    and its type from int to list of int. This needs a protocol version
    increase in order to avoid incompatibilities.
    
    Reviewed-by: imsnah

--- lib/constants.py
+++ lib/constants.py
@@ -25,7 +25,7 @@ from ganeti import _autoconf
 
 # various versions
 CONFIG_VERSION = 3
-PROTOCOL_VERSION = 14
+PROTOCOL_VERSION = 15
 RELEASE_VERSION = _autoconf.PACKAGE_VERSION
 OS_API_VERSION = 5
 EXPORT_VERSION = 0
