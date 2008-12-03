4ce6007f69e11478085142420c975158e78ede6b
Michael Hanselmann <hansmi@google.com>
2008-12-03 16:09:46 +0000

    ganeti.http: Fix copyright header
    Reviewed-by: ultrotter

--- lib/http.py
+++ lib/http.py
@@ -1,5 +1,8 @@
 #
 #
+
+# Copyright (C) 2007, 2008 Google Inc.
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
