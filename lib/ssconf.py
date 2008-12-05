02b31f3219674fc8411d4e08dc8b7b4e0c6d5bdb
Iustin Pop <iustin@google.com>
2008-12-05 02:42:18 +0000

    ssconf: empty files should not add a newline
    Currently we add a newline in the ssconf writeout process, even if the
    file is empty. We chage this case so that lists of values (e.g. offline
    nodes) are correct (not a list of one empty element).
    
    Reviewed-by: imsnah

--- lib/ssconf.py
+++ lib/ssconf.py
@@ -185,7 +185,7 @@ class SimpleStore(object):
     ssconf_lock.Exclusive(blocking=True, timeout=SSCONF_LOCK_TIMEOUT)
     try:
       for name, value in values.iteritems():
-        if not value.endswith("\n"):
+        if value and not value.endswith("\n"):
           value += "\n"
         utils.WriteFile(self.KeyToFilename(name), data=value)
     finally:
