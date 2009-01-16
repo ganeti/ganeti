c6ea14cb6a5f6b3899de62ecfa6f0fa38f538b93
Iustin Pop <iustin@google.com>
2009-01-16 16:26:03 +0000

    Fix LUExportInstance
    Due to deficiencies in our block device implementation, it is a must to
    call SetDiskID on disks before passing them to remote nodes. Since in
    export/import, we don't touch the disks themselves, this was not needed
    before in this function.
    
    However, since having instance symlinks, the correct ID is needed here
    too, and with static minors it's a "must need". This reflects into
    failed instance starts after migration and/or failover.
    
    Reviewed-by: ultrotter

--- lib/cmdlib.py
+++ lib/cmdlib.py
@@ -5376,6 +5376,11 @@ class LUExportInstance(LogicalUnit):
 
     snap_disks = []
 
+    # set the disks ID correctly since call_instance_start needs the
+    # correct drbd minor to create the symlinks
+    for disk in instance.disks:
+      self.cfg.SetDiskID(disk, src_node)
+
     try:
       for disk in instance.disks:
         if disk.iv_name == "sda":
