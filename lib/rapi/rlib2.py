ganeti/ganeti

commit 7a95a954decb4c038ed8af4949fac9268bacfc33

rapi: Add /2/nodes/[node_name]/storage resource
Signed-off-by: Michael Hanselmann <hansmi@google.com>
Reviewed-by: Guido Trotter <ultrotter@google.com>

lib/rapi/rlib2.py
@@ -281,7 +281,7 @@ class R_2_nodes_name_evacuate(baserlib.R_Generic):
 
 
 class R_2_nodes_name_migrate(baserlib.R_Generic):
-  """/2/nodes/[node_name]/evacuate migrate.
+  """/2/nodes/[node_name]/migrate resource.
 
   """
   def POST(self):
@@ -293,6 +293,32 @@ class R_2_nodes_name_migrate(baserlib.R_Generic):
 
     op = opcodes.OpMigrateNode(node_name=node_name, live=live)
 
+    return baserlib.SubmitJob([op])
+
+
+class R_2_nodes_name_storage(baserlib.R_Generic):
+  """/2/nodes/[node_name]/storage ressource.
+
+  """
+  # LUQueryNodeStorage acquires locks, hence restricting access to GET
+  GET_ACCESS = [rapi.RAPI_ACCESS_WRITE]
+
+  def GET(self):
+    node_name = self.items[0]
+
+    storage_type = self._checkStringVariable("storage_type", None)
+    if not storage_type:
+      raise http.HttpBadRequest("Missing the required 'storage_type'"
+                                " parameter")
+
+    output_fields = self._checkStringVariable("output_fields", None)
+    if not output_fields:
+      raise http.HttpBadRequest("Missing the required 'output_fields'"
+                                " parameter")
+
+    op = opcodes.OpQueryNodeStorage(nodes=[node_name],
+                                    storage_type=storage_type,
+                                    output_fields=output_fields.split(","))
     return baserlib.SubmitJob([op])
 
 

lib/rapi/connector.py
@@ -159,6 +159,8 @@ CONNECTOR.update({
       rlib2.R_2_nodes_name_evacuate,
   re.compile(r'^/2/nodes/([\w\._-]+)/migrate$'):
       rlib2.R_2_nodes_name_migrate,
+  re.compile(r'^/2/nodes/([\w\._-]+)/storage$'):
+      rlib2.R_2_nodes_name_storage,
   "/2/instances": rlib2.R_2_instances,
   re.compile(r'^/2/instances/([\w\._-]+)$'): rlib2.R_2_instances_name,
   re.compile(r'^/2/instances/([\w\._-]+)/tags$'): rlib2.R_2_instances_name_tags,

commit 8381fa2d54c5fa3526d04d0aed45ed2655b8b2f7

rlib2: Import rapi
This fixes ganeti-rapi which was broken in commit 7a95a954.

Signed-off-by: Michael Hanselmann <hansmi@google.com>
Reviewed-by: Iustin Pop <iustin@google.com>

lib/rapi/rlib2.py
@@ -27,6 +27,7 @@ from ganeti import opcodes
 from ganeti import http
 from ganeti import constants
 from ganeti import cli
+from ganeti import rapi
 from ganeti.rapi import baserlib
 
 