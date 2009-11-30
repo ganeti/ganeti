313b2dd43c1a958abdab798558a85b3d3bc8aa2e
Michael Hanselmann <hansmi@google.com>
2009-11-30 14:09:28 +0100

    gnt-cluster verify: Warn if node time diverges too far
    The warning will be generated if the clocks diverge by more
    than 150 seconds. Due to the way the RPC system works, we
    cannot get exact time differences, e.g. if one of the
    queried nodes is broken. The comparision is done using a
    time window.
    
    Confd queries will fail if the clock on the client and server
    are more than 300 seconds from each other. This check helps
    keeping at least the nodes of a cluster in sync.
    
    Signed-off-by: Michael Hanselmann <hansmi@google.com>
    Reviewed-by: Guido Trotter <ultrotter@google.com>

--- lib/constants.py
+++ lib/constants.py
@@ -320,7 +320,7 @@ DEFAULT_MAC_PREFIX = "aa:00:00"
 LVM_STRIPECOUNT = _autoconf.LVM_STRIPECOUNT
 # default maximum instance wait time, in seconds.
 DEFAULT_SHUTDOWN_TIMEOUT = 120
-
+NODE_MAX_CLOCK_SKEW = 150
 
 # RPC constants
 (RPC_ENCODING_NONE,
@@ -526,6 +526,7 @@ NV_LVLIST = "lvlist"
 NV_PVLIST = "pvlist"
 NV_DRBDLIST = "drbd-list"
 NV_NODESETUP = "nodesetup"
+NV_TIME = "time"
 
 # Allocator framework constants
 IALLOCATOR_VERSION = 2
@@ -722,7 +723,7 @@ CONFD_ERROR_ARGUMENT = 3
 # Each request is "salted" by the current timestamp.
 # This constants decides how many seconds of skew to accept.
 # TODO: make this a default and allow the value to be more configurable
-CONFD_MAX_CLOCK_SKEW = 300
+CONFD_MAX_CLOCK_SKEW = 2 * NODE_MAX_CLOCK_SKEW
 
 # When we haven't reloaded the config for more than this amount of seconds, we
 # force a test to see if inotify is betraying us.
