ganeti/ganeti

commit 413b747225f15fe8574d07ba696435d25767d603

Fix locking bug causing high CPU usage
Iustin Pop noticed unusually high CPU usage with 2.1's master
daemon, even with very simple opcodes like OP_TEST_DELAY. As
it turns out, we inadvertently passed seconds as milliseconds
to a call to poll(2). Due to the way the loop around the call
works it didn't break competely, but caused higher CPU usage
by the poll(2) call returning too early.

Signed-off-by: Michael Hanselmann <hansmi@google.com>
Reviewed-by: Iustin Pop <iustin@google.com>

lib/locking.py
@@ -145,7 +145,8 @@ class _SingleNotifyPipeConditionWaiter(object):
         break
 
       try:
-        result = self._poller.poll(remaining_time)
+        # Our calculation uses seconds, poll() wants milliseconds
+        result = self._poller.poll(1000 * remaining_time)
       except EnvironmentError, err:
         if err.errno != errno.EINTR:
           raise

commit b44b01415f85574f59f1a67ea95a5504f3cac6b8

Fix bug introduced in commit 413b747
While commit 413b747 fixed the issue of poll(2) returning too
soon, it didn't work when the poll(2) call should've been
blocking. This is now fixed and verified.

Signed-off-by: Michael Hanselmann <hansmi@google.com>
Reviewed-by: Iustin Pop <iustin@google.com>

lib/locking.py
@@ -141,12 +141,15 @@ class _SingleNotifyPipeConditionWaiter(object):
     while True:
       remaining_time = running_timeout.Remaining()
 
-      if remaining_time is not None and remaining_time < 0.0:
-        break
+      if remaining_time is not None:
+        if remaining_time < 0.0:
+          break
+
+        # Our calculation uses seconds, poll() wants milliseconds
+        remaining_time *= 1000
 
       try:
-        # Our calculation uses seconds, poll() wants milliseconds
-        result = self._poller.poll(1000 * remaining_time)
+        result = self._poller.poll(remaining_time)
       except EnvironmentError, err:
         if err.errno != errno.EINTR:
           raise