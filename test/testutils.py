ganeti/ganeti

commit 70b0d2a29bb50277a8cf8fe2820fa75326fc64cf

Ensure all resources are used by RAPI client
Signed-off-by: Michael Hanselmann <hansmi@google.com>
Reviewed-by: René Nussbaumer <rn@google.com>

test/ganeti.rapi.client_unittest.py
@@ -30,6 +30,7 @@ import pycurl
 from ganeti import constants
 from ganeti import http
 from ganeti import serializer
+from ganeti import utils
 
 from ganeti.rapi import connector
 from ganeti.rapi import rlib2
@@ -39,6 +40,15 @@ import testutils
 
 
 _URI_RE = re.compile(r"https://(?P<host>.*):(?P<port>\d+)(?P<path>/.*)")
+
+# List of resource handlers which aren't used by the RAPI client
+_KNOWN_UNUSED = set([
+  connector.R_root,
+  connector.R_2,
+  ])
+
+# Global variable for collecting used handlers
+_used_handlers = None
 
 
 def _GetPathFromUri(uri):
@@ -107,8 +117,12 @@ class RapiMock(object):
     self._last_req_data = request_body
 
     try:
-      HandlerClass, items, args = self._mapper.getController(path)
-      self._last_handler = HandlerClass(items, args, None)
+      (handler_cls, items, args) = self._mapper.getController(path)
+
+      # Record handler as used
+      _used_handlers.add(handler_cls)
+
+      self._last_handler = handler_cls(items, args, None)
       if not hasattr(self._last_handler, method.upper()):
         raise http.HttpNotImplemented(message="Method not implemented")
 
@@ -1130,5 +1144,27 @@ class GanetiRapiClientTests(testutils.GanetiTestCase):
       self.assertEqual(self.rapi.CountPending(), 0)
 
 
+class RapiTestRunner(unittest.TextTestRunner):
+  def run(self, *args):
+    global _used_handlers
+    assert _used_handlers is None
+
+    _used_handlers = set()
+    try:
+      # Run actual tests
+      result = unittest.TextTestRunner.run(self, *args)
+
+      diff = (set(connector.CONNECTOR.values()) - _used_handlers -
+             _KNOWN_UNUSED)
+      if diff:
+        raise AssertionError("The following RAPI resources were not used by the"
+                             " RAPI client: %r" % utils.CommaJoin(diff))
+    finally:
+      # Reset global variable
+      _used_handlers = None
+
+    return result
+
+
 if __name__ == '__main__':
-  client.UsesRapiClient(testutils.GanetiTestProgram)()
+  client.UsesRapiClient(testutils.GanetiTestProgram)(testRunner=RapiTestRunner)

commit c6a65efb455352087ee2b10c3a014e7303b6284c

Fix unittest breakage on Python 2.4/2.5
Commit 70b0d2a29 broke unittests on Python 2.4 and 2.5. Turns out that
Python 2.6 and above allow classes to be passed as custom test runners,
whereas earlier versions don't.

Signed-off-by: Michael Hanselmann <hansmi@google.com>
Reviewed-by: Iustin Pop <iustin@google.com>

test/testutils.py
@@ -27,6 +27,7 @@ import stat
 import tempfile
 import unittest
 import logging
+import types
 
 from ganeti import utils
 
@@ -77,6 +78,18 @@ class GanetiTestProgram(unittest.TestProgram):
       pass
     else:
       raise Exception("Assertion not evaluated")
+
+    # The following piece of code is a backport from Python 2.6. Python 2.4/2.5
+    # only accept class instances as test runners. Being able to pass classes
+    # reduces the amount of code necessary for using a custom test runner.
+    # 2.6 and above should use their own code, however.
+    if (self.testRunner and sys.hexversion < 0x2060000 and
+        isinstance(self.testRunner, (type, types.ClassType))):
+      try:
+        self.testRunner = self.testRunner(verbosity=self.verbosity)
+      except TypeError:
+        # didn't accept the verbosity argument
+        self.testRunner = self.testRunner()
 
     return unittest.TestProgram.runTests(self)
 