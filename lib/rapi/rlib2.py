63ea9789fc27df324b957784e21316ed8f3bd33a
Iustin Pop <iustin@google.com>
2011-03-04 11:04:10 +0100

    RAPI: fix evacuate node resource
    PollJob returns the whole op_results, hence a list of opcode results.
    
    Signed-off-by: Iustin Pop <iustin@google.com>
    Reviewed-by: René Nussbaumer <rn@google.com>

--- lib/rapi/rlib2.py
+++ lib/rapi/rlib2.py
@@ -1,7 +1,7 @@
 #
 #
 
-# Copyright (C) 2006, 2007, 2008, 2009, 2010 Google Inc.
+# Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011 Google Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -403,7 +403,7 @@ class R_2_nodes_name_evacuate(baserlib.R_Generic):
     result = cli.PollJob(job_id, cl, feedback_fn=baserlib.FeedbackFn)
 
     jobs = []
-    for iname, node in result:
+    for iname, node in result[0]:
       if dry_run:
         jid = None
       else:
