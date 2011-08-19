f299ca219577662b6ee37639f891a78bdfdc6369
Michael Hanselmann <hansmi@google.com>
2011-08-19 16:33:48 +0200

    Unify some file headers
    Remove unnecessary commas, add empty lines where necessary to make them
    consistent.
    
    I'm working on a script to check this, but it's not yet ready.
    
    Signed-off-by: Michael Hanselmann <hansmi@google.com>
    Reviewed-by: René Nussbaumer <rn@google.com>

--- lib/confd/querylib.py
+++ lib/confd/querylib.py
@@ -1,7 +1,7 @@
 #
 #
 
-# Copyright (C) 2009, Google Inc.
+# Copyright (C) 2009 Google Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
