d5a2a5500ee4d78c6e53d8e6bf8bfa42b4cb6189
Michael Hanselmann <hansmi@google.com>
2011-10-21 15:28:05 +0200

    Add script to generate RPC wrappers
    A new script, “autotools/build-rpc”, will be used to generate code for
    RPC client wrappers. This is done because “lib/rpc.py” contains lots and
    lots of boilerplate code. Forthcoming patches will start converting
    RPC wrappers.
    
    Signed-off-by: Michael Hanselmann <hansmi@google.com>
    Reviewed-by: Iustin Pop <iustin@google.com>

---
+++ lib/build/rpc_definitions.py
@@ -0,0 +1,40 @@
+#
+#
+
+# Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011 Google Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
+"""RPC definitions for communication between master and node daemons.
+
+"""
+
+
+# Various time constants for the timeout table
+TMO_URGENT = 60 # one minute
+TMO_FAST = 5 * 60 # five minutes
+TMO_NORMAL = 15 * 60 # 15 minutes
+TMO_SLOW = 3600 # one hour
+TMO_4HRS = 4 * 3600
+TMO_1DAY = 86400
+
+SINGLE = "single-node"
+MULTI = "multi-node"
+
+CALLS = {
+  "RpcClientDefault": [
+    ],
+  }
