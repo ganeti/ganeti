a4c0fe1e147840f24b5f866e4ebbc7e3bf71cd87
Iustin Pop <iustin@google.com>
2012-11-12 12:14:37 +0100

    Fix PID file writing in Haskell daemons
    Currently, the code uses createFile, which has the effect of always
    truncating the file. This is bad, as the content of the PID file is
    wiped even when we wouldn't be able to lock it!
    
    We switch to openFd (createFile is just a wrapper over that), and we
    use an explicit set of flags; defaultFileFlags is already safe
    (trunc=False), but I prefer to set it explicitly with our desired
    flags.
    
    Note that this bug doesn't manifest in normal usage, as daemon-util
    won't try to start the daemon if already running. But if anyone or
    anything does call ganeti-confd explicitly, the pid file will be
    emptied and the daemon will keep trying to be restarted forever…
    
    Signed-off-by: Iustin Pop <iustin@google.com>
    Reviewed-by: Guido Trotter <ultrotter@google.com>

--- htools/Ganeti/Daemon.hs
+++ htools/Ganeti/Daemon.hs
@@ -196,14 +196,19 @@ parseArgs cmd options = do
   parseOpts cmd_args cmd options
 
 -- * Daemon-related functions
+
 -- | PID file mode.
 pidFileMode :: FileMode
 pidFileMode = unionFileModes ownerReadMode ownerWriteMode
 
+-- | PID file open flags.
+pidFileFlags :: OpenFileFlags
+pidFileFlags = defaultFileFlags { noctty = True, trunc = False }
+
 -- | Writes a PID file and locks it.
 _writePidFile :: FilePath -> IO Fd
 _writePidFile path = do
-  fd <- createFile path pidFileMode
+  fd <- openFd path ReadWrite (Just pidFileMode) pidFileFlags
   setLock fd (WriteLock, AbsoluteSeek, 0, 0)
   my_pid <- getProcessID
   _ <- fdWrite fd (show my_pid ++ "\n")
