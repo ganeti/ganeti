ganeti/ganeti

commit a8b3b09dc9b6eb6144c9c471cb7e1ecae9751ebd

Factorize SSL context setup for certificate check
This code will also be used by the node daemon setup utility.

Signed-off-by: Michael Hanselmann <hansmi@google.com>
Reviewed-by: Helga Velroyen <helgav@google.com>

lib/utils/x509.py
@@ -319,3 +319,22 @@ def ExtractX509Certificate(pem):
 
   return (cert,
           OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, cert))
+
+
+def PrepareX509CertKeyCheck(cert, key):
+  """Get function for verifying certificate with a certain private key.
+
+  @type key: OpenSSL.crypto.PKey
+  @param key: Private key object
+  @type cert: OpenSSL.crypto.X509
+  @param cert: X509 certificate object
+  @rtype: callable
+  @return: Callable doing the actual check; will raise C{OpenSSL.SSL.Error} if
+    certificate is not signed by given private key
+
+  """
+  ctx = OpenSSL.SSL.Context(OpenSSL.SSL.TLSv1_METHOD)
+  ctx.use_certificate(cert)
+  ctx.use_privatekey(key)
+
+  return ctx.check_privatekey

lib/tools/prepare_node_join.py
@@ -130,11 +130,9 @@ def _VerifyCertificate(cert, _noded_cert_file=pathutils.NODED_CERT_FILE):
     raise errors.X509CertError(_noded_cert_file,
                                "Unable to load private key: %s" % err)
 
-  ctx = OpenSSL.SSL.Context(OpenSSL.SSL.TLSv1_METHOD)
-  ctx.use_privatekey(key)
-  ctx.use_certificate(cert)
+  check_fn = utils.PrepareX509CertKeyCheck(cert, key)
   try:
-    ctx.check_privatekey()
+    check_fn()
   except OpenSSL.SSL.Error:
     raise JoinError("Given cluster certificate does not match local key")
 

commit 86b9a3854c8cd0921f27b7e6d989359210b6c357

Fix breakage introduced in commit a8b3b09
The order of the calls to “ctx.use_privatekey” and “ctx.use_certificate”
was wrong, leading to an exception being thrown.

Signed-off-by: Michael Hanselmann <hansmi@google.com>
Reviewed-by: Guido Trotter <ultrotter@google.com>

lib/utils/x509.py
@@ -334,7 +334,7 @@ def PrepareX509CertKeyCheck(cert, key):
 
   """
   ctx = OpenSSL.SSL.Context(OpenSSL.SSL.TLSv1_METHOD)
-  ctx.use_certificate(cert)
   ctx.use_privatekey(key)
+  ctx.use_certificate(cert)
 
   return ctx.check_privatekey