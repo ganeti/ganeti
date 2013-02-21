dcd85eef75c1edc2be26e8cbd228d7087ba8c5bf
Michael Hanselmann <hansmi@google.com>
2013-02-21 15:32:51 +0100

    QA: Read virtual node's RAPI certificate
    The virtual node prefix must be added to the path.
    
    Signed-off-by: Michael Hanselmann <hansmi@google.com>
    Reviewed-by: Bernardo Dal Seno <bdalseno@google.com>

--- qa/qa_rapi.py
+++ qa/qa_rapi.py
@@ -76,7 +76,7 @@ def Setup(username, password):
   master = qa_config.GetMasterNode()
 
   # Load RAPI certificate from master node
-  cmd = ["cat", pathutils.RAPI_CERT_FILE]
+  cmd = ["cat", qa_utils.MakeNodePath(master, pathutils.RAPI_CERT_FILE)]
 
   # Write to temporary file
   _rapi_ca = tempfile.NamedTemporaryFile()
