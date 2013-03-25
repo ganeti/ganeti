dbae804adc97cb1fa73c789bc9f424e8d409f086
Michele Tartara <mtartara@google.com>
2013-03-25 18:24:51 +0100

    Add a field to store the reason trail in OpCodes
    The reason trail will be available for all the OpCodes, so the field that
    is to contain it has to be added to the OpCode base class.
    
    Signed-off-by: Michele Tartara <mtartara@google.com>
    Reviewed-by: Helga Velroyen <helgav@google.com>

--- lib/opcodes.py
+++ lib/opcodes.py
@@ -659,6 +659,8 @@ class OpCode(BaseOpCode):
      " for details"),
     (COMMENT_ATTR, None, ht.TMaybeString,
      "Comment describing the purpose of the opcode"),
+    (constants.OPCODE_REASON, None, ht.TMaybeList,
+     "The reason trail, describing why the OpCode is executed"),
     ]
   OP_RESULT = None
 
