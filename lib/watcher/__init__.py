9598b71fba771ee17c73494e87bd47781be09c51
Michele Tartara <mtartara@google.com>
2013-05-14 18:26:07 +0100

    Remove extra newline
    Also, properly set the date of the last modification.
    
    Signed-off-by: Michele Tartara <mtartara@google.com>
    Reviewed-by: Helga Velroyen <helgav@google.com>

--- lib/watcher/__init__.py
+++ lib/watcher/__init__.py
@@ -1,7 +1,7 @@
 #
 #
 
-# Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012 Google Inc.
+# Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 Google Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -99,7 +99,6 @@ def StartNodeDaemons():
   # start mond as well: all nodes need monitoring
   if constants.ENABLE_MOND:
     utils.EnsureDaemon(constants.MOND)
-
 
 
 def RunWatcherHooks():
