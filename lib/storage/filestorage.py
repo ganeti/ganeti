f3ebe73e09d8e4ab4f7409892102284175a7a051
Helga Velroyen <helgav@google.com>
2013-07-17 15:47:22 +0200

    Move bdev unit tests to filestorage unit tests
    Some code was moved from bdev to file storage in
    previous patches, but the related unit tests were
    accidentally left in bdev. Moving and adjusting
    them now.
    
    Signed-off-by: Helga Velroyen <helgav@google.com>
    Reviewed-by: Klaus Aehlig <aehlig@google.com>

--- lib/storage/filestorage.py
+++ lib/storage/filestorage.py
@@ -167,6 +167,9 @@ def CheckFileStoragePathAcceptance(
 
   """
   allowed = _LoadAllowedFileStoragePaths(_filename)
+  if not allowed:
+    raise errors.FileStoragePathError("No paths are valid or path file '%s'"
+                                      " was not accessible." % _filename)
 
   if _ComputeWrongFileStoragePaths([path]):
     raise errors.FileStoragePathError("Path '%s' uses a forbidden prefix" %
@@ -185,7 +188,7 @@ def _CheckFileStoragePathExistance(path):
 
   """
   if not os.path.isdir(path):
-    raise errors.FileStoragePathError("Path '%s' is not exisiting or not a"
+    raise errors.FileStoragePathError("Path '%s' is not existing or not a"
                                       " directory." % path)
   if not os.access(path, os.W_OK):
     raise errors.FileStoragePathError("Path '%s' is not writable" % path)
