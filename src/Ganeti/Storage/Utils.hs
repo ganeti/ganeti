ganeti/ganeti

commit ede6df3d02a0968c12e4440423eb49abf8eb28d8

Remove duplicate storage units in node query
This is a little performance tweak for the node queries.
So far, the query code mapped the disk templates to storage
units. It could happen that two disk templates were mapped
to the same storage unit and therefore the storage space
information was calculated twice for the same unit. This
patch removes this unecessary double calculation by
filtering for duplicate storage units before invoking the
the RPC call.

Signed-off-by: Helga Velroyen <helgav@google.com>
Reviewed-by: Klaus Aehlig <aehlig@google.com>

src/Ganeti/Storage/Utils.hs
@@ -34,6 +34,7 @@ import Ganeti.Types
 import qualified Ganeti.Types as T
 
 import Control.Monad
+import Data.List (nub)
 import Data.Maybe
 import qualified Data.Map as M
 
@@ -56,7 +57,7 @@ getDefaultSpindleSU cfg =
 getClusterStorageUnitRaws :: ConfigData -> [StorageUnitRaw]
 getClusterStorageUnitRaws cfg = foldSUs (maybe_units ++ [spindle_unit])
   where disk_templates = clusterEnabledDiskTemplates $ configCluster cfg
-        storage_types = map diskTemplateToStorageType disk_templates
+        storage_types = nub $ map diskTemplateToStorageType disk_templates
         maybe_units = zip storage_types (map (getDefaultStorageKey cfg)
             disk_templates)
         spindle_unit = getDefaultSpindleSU cfg

commit fa7f3e26fcd3f7cfa9eee30b83a039f0deef5e64

Fix removal of duplicates
Commit ede6df3d02 introduced a bug in the node queries
where disk templates where paired up wrongly to their
storage unit keys due to removal of duplicates at the
wrong place. This patch fixes it.

Signed-off-by: Helga Velroyen <helgav@google.com>
Reviewed-by: Klaus Aehlig <aehlig@google.com>

src/Ganeti/Storage/Utils.hs
@@ -55,9 +55,10 @@ getDefaultSpindleSU cfg =
 
 -- | Get the cluster's storage units from the configuration
 getClusterStorageUnitRaws :: ConfigData -> [StorageUnitRaw]
-getClusterStorageUnitRaws cfg = foldSUs (maybe_units ++ [spindle_unit])
+getClusterStorageUnitRaws cfg =
+    foldSUs (nub (maybe_units ++ [spindle_unit]))
   where disk_templates = clusterEnabledDiskTemplates $ configCluster cfg
-        storage_types = nub $ map diskTemplateToStorageType disk_templates
+        storage_types = map diskTemplateToStorageType disk_templates
         maybe_units = zip storage_types (map (getDefaultStorageKey cfg)
             disk_templates)
         spindle_unit = getDefaultSpindleSU cfg