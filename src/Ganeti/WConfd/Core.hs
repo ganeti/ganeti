13f59e11c531bcecacad29e8d4733aac6bc49143
Petr Pudlak <pudlak@google.com>
2014-01-07 13:41:48 +0100

    Add a module for WConfd core functions
    There are no exported functions yet, except for `echo`, which just
    returns its argument back to a client.
    
    Signed-off-by: Petr Pudlak <pudlak@google.com>
    Reviewed-by: Klaus Aehlig <aehlig@google.com>

---
+++ src/Ganeti/WConfd/Core.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+{-| The Ganeti WConfd core functions.
+
+As TemplateHaskell require that splices be defined in a separate
+module, we combine all the TemplateHaskell functionality that HTools
+needs in this module (except the one for unittests).
+
+-}
+
+{-
+
+Copyright (C) 2013 Google Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.
+
+-}
+
+module Ganeti.WConfd.Core where
+
+import Language.Haskell.TH (Name)
+
+import Ganeti.WConfd.Monad
+
+-- * The list of all functions exported to RPC.
+
+-- Just a test function
+echo :: String -> WConfdMonad String
+echo = return
+
+exportedFunctions :: [Name]
+exportedFunctions = [ 'echo ]
