52261ad2de0cd3fc2445c9233ee6aff5b4dbb737
Klaus Aehlig <aehlig@google.com>
2014-01-09 13:58:03 +0100

    Remove certification on 2.11 to 2.10 downgrade
    While version 2.10 ignores any leftover client certificates, their
    presence will prevent a the cluster working after an upgrade back
    to version 2.11 again. So we have to remove them right at the
    downgrade.
    
    Signed-off-by: Klaus Aehlig <aehlig@google.com>
    Reviewed-by: Helga Velroyen <helgav@google.com>

--- lib/client/gnt_cluster.py
+++ lib/client/gnt_cluster.py
@@ -1881,6 +1881,22 @@ def _VersionSpecificDowngrade():
   """
   ToStdout("Performing version-specific downgrade tasks.")
 
+  ToStdout("...removing client certificates ssconf file")
+  ssconffile = ssconf.SimpleStore().KeyToFilename(
+    constants.SS_MASTER_CANDIDATES_CERTS)
+  badnodes = _VerifyCommand(["rm", "-f", ssconffile])
+  if badnodes:
+    ToStderr("Warning: failed to clean up ssconf on %s."
+             % (", ".join(badnodes),))
+    return False
+
+  ToStdout("...removing client certificates")
+  badnodes = _VerifyCommand(["rm", "-f", pathutils.NODED_CLIENT_CERT_FILE])
+  if badnodes:
+    ToStderr("Warning: failed to clean up certificates on %s."
+             % (", ".join(badnodes),))
+    return False
+
   return True
 
 
