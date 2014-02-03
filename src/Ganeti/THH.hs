f20038fdbb45b506361cd74114e6a7fe674ca9f8
Petr Pudlak <pudlak@google.com>
2014-02-03 14:19:29 +0100

    Another workaround for GHC linking error "unknown symbol"
    This error appears after adding the dependency to lifted-base.
    See also #683 and ee7caf27.
    
    I surmise that something like this is happening: Some code, that uses
    the library, is only referenced through Template Haskell. A probable bug
    in GHC linker fails to notice it and doesn't include the library when
    linking, causing the error. Referencing the library explicitly in THH.hs
    solves the problem.
    
    Signed-off-by: Petr Pudlak <pudlak@google.com>
    Reviewed-by: Klaus Aehlig <aehlig@google.com>

--- src/Ganeti/THH.hs
+++ src/Ganeti/THH.hs
@@ -73,6 +73,7 @@ module Ganeti.THH ( declareSADT
 import Control.Arrow ((&&&))
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Base () -- Needed to prevent spurious GHC linking errors.
 import Data.Attoparsec () -- Needed to prevent spurious GHC 7.4 linking errors.
   -- See issue #683 and https://ghc.haskell.org/trac/ghc/ticket/4899
 import Data.Char
