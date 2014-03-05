ba40281b257423d9e78b5dda8c44cebf352354cb
Petr Pudlak <pudlak@google.com>
2014-03-05 14:48:42 +0100

    Add a module for custom generic Lens functions
    The module currently re-exports Control.Lens and adds a function that
    creates lenses from data types by appending 'L' to its field names.
    
    All Ganeti modules should import this module instead of Control.Lens
    directly.
    
    Signed-off-by: Petr Pudlak <pudlak@google.com>
    Reviewed-by: Klaus Aehlig <aehlig@google.com>

---
+++ src/Ganeti/Lens.hs
@@ -0,0 +1,57 @@
+{-| Provides all lens-related functions.
+
+-}
+
+{-
+
+Copyright (C) 2014 Google Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.
+
+-}
+
+module Ganeti.Lens
+  ( module Control.Lens
+  , makeCustomLenses
+  , makeCustomLenses'
+  ) where
+
+import Control.Lens
+import Control.Monad
+import qualified Data.Set as S
+import Language.Haskell.TH
+
+lensFieldName :: String -> String
+lensFieldName = (++ "L")
+
+-- | Internal helper method for constructing partial set of lenses.
+makeCustomLensesFiltered :: (String -> Bool) -> Name -> Q [Dec]
+makeCustomLensesFiltered f = makeLensesWith customRules
+  where
+    customRules :: LensRules
+    customRules = set lensField (fmap lensFieldName . mfilter f . Just)
+                      defaultRules
+
+-- | Create lenses for all fields of a given data type.
+makeCustomLenses :: Name -> Q [Dec]
+makeCustomLenses = makeCustomLensesFiltered (const True)
+
+-- | Create lenses for some fields of a given data type.
+makeCustomLenses' :: Name -> [Name] -> Q [Dec]
+makeCustomLenses' name lst = makeCustomLensesFiltered f name
+  where
+    allowed = S.fromList . map nameBase $ lst
+    f = flip S.member allowed
