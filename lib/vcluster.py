127266b4a7264836fb5da3297c4e89b3b56068a8
Petr Pudlak <pudlak@google.com>
2014-03-06 13:51:53 +0100

    Add more meaningful error messages to asserts in vcluster
    .. to simplify debugging of RPC calls.
    
    Signed-off-by: Petr Pudlak <pudlak@google.com>
    Reviewed-by: Klaus Aehlig <aehlig@google.com>

--- lib/vcluster.py
+++ lib/vcluster.py
@@ -111,7 +111,7 @@ def _PreparePaths(rootdir, hostname):
                        (_ROOTDIR_ENVNAME, _HOSTNAME_ENVNAME))
 
   if rootdir:
-    assert rootdir == os.path.normpath(rootdir)
+    assert rootdir == os.path.normpath(rootdir), "Not normalized: " + rootdir
 
     _CheckHostname(hostname)
 
@@ -191,14 +191,14 @@ def AddNodePrefix(path, _noderoot=_VIRT_NODEROOT):
   directory specified in the environment.
 
   """
-  assert os.path.isabs(path)
+  assert os.path.isabs(path), "Path not absolute: " + path
 
   if _noderoot:
     result = "%s/%s" % (_noderoot, path)
   else:
     result = path
 
-  assert os.path.isabs(result)
+  assert os.path.isabs(result), "Path not absolute: " + path
 
   return os.path.normpath(result)
 
@@ -210,7 +210,7 @@ def _RemoveNodePrefix(path, _noderoot=_VIRT_NODEROOT):
   path.
 
   """
-  assert os.path.isabs(path)
+  assert os.path.isabs(path), "Path not absolute: " + path
 
   norm_path = os.path.normpath(path)
 
@@ -228,7 +228,7 @@ def _RemoveNodePrefix(path, _noderoot=_VIRT_NODEROOT):
   else:
     result = norm_path
 
-  assert os.path.isabs(result)
+  assert os.path.isabs(result), "Path not absolute: " + path
 
   return result
 
@@ -241,7 +241,7 @@ def MakeVirtualPath(path, _noderoot=_VIRT_NODEROOT):
   undo the process. Virtual paths are meant to be transported via RPC.
 
   """
-  assert os.path.isabs(path)
+  assert os.path.isabs(path), "Path not absolute: " + path
 
   if _noderoot and path not in _VPATH_WHITELIST:
     return _VIRT_PATH_PREFIX + _RemoveNodePrefix(path, _noderoot=_noderoot)
@@ -257,7 +257,7 @@ def LocalizeVirtualPath(path, _noderoot=_VIRT_NODEROOT):
   Virtual paths are meant to be transported via RPC.
 
   """
-  assert os.path.isabs(path)
+  assert os.path.isabs(path), "Path not absolute: " + path
 
   if _noderoot and path not in _VPATH_WHITELIST:
     if path.startswith(_VIRT_PATH_PREFIX):
