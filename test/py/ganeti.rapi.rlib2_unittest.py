4589d0e63eeae03ea77fb3f0d37f23ea4116a846
Hrvoje Ribicic <riba@google.com>
2014-03-25 15:19:32 +0000

    Add test for group custom parameter renaming
    This patch introduces a simple test checking if a custom_* parameter is
    renamed and passed to the opcode correctly.
    
    Signed-off-by: Hrvoje Ribicic <riba@google.com>
    Reviewed-by: Thomas Thrainer <thomasth@google.com>

--- test/py/ganeti.rapi.rlib2_unittest.py
+++ test/py/ganeti.rapi.rlib2_unittest.py
@@ -1588,6 +1588,40 @@ class TestGroupModify(unittest.TestCase):
     self.assertFalse(hasattr(op, "dry_run"))
     self.assertRaises(IndexError, cl.GetNextSubmittedJob)
 
+  def testCustomParamRename(self):
+    clfactory = _FakeClientFactory(_FakeClient)
+
+    name = "groupie"
+    data = {
+      "custom_diskparams": {},
+      "custom_ipolicy": {},
+      "custom_ndparams": {},
+      }
+
+    handler = _CreateHandler(rlib2.R_2_groups_name_modify, [name], {}, data,
+                             clfactory)
+    job_id = handler.PUT()
+
+    cl = clfactory.GetNextClient()
+    self.assertRaises(IndexError, clfactory.GetNextClient)
+
+    (exp_job_id, (op, )) = cl.GetNextSubmittedJob()
+    self.assertEqual(job_id, exp_job_id)
+
+    self.assertTrue(isinstance(op, opcodes.OpGroupSetParams))
+    self.assertEqual(op.diskparams, {})
+    self.assertEqual(op.ipolicy, {})
+    self.assertEqual(op.ndparams, {})
+
+    self.assertRaises(IndexError, cl.GetNextSubmittedJob)
+
+    # Define both
+    data["diskparams"] = {}
+    assert "diskparams" in data and "custom_diskparams" in data
+    handler = _CreateHandler(rlib2.R_2_groups_name_modify, [name], {}, data,
+                             clfactory)
+    self.assertRaises(http.HttpBadRequest, handler.PUT)
+
 
 class TestGroupAdd(unittest.TestCase):
   def test(self):
