da5d043d92cb80c872f470463bcbbcfc4661dfda
Klaus Aehlig <aehlig@google.com>
2014-05-13 19:07:28 +0200

    Fix hard-coded list of daemons in test
    The reality is, that masterd is gone now, so there is
    no point in testing that daemon-utils knows about it.
    In fact, it would be a bug if daemon-util would mention
    it.
    
    Signed-off-by: Klaus Aehlig <aehlig@google.com>
    Reviewed-by: Hrvoje Ribicic <riba@google.com>

--- test/py/daemon-util_unittest.bash
+++ test/py/daemon-util_unittest.bash
@@ -36,8 +36,8 @@ if ! grep -q '^ENABLE_MOND = ' lib/_constants.py; then
   err "Please update $0, mond enable feature is missing"
 fi
 
-DAEMONS_LIST="wconfd noded masterd rapi luxid"
-STOPDAEMONS_LIST="luxid rapi masterd noded wconfd"
+DAEMONS_LIST="wconfd noded rapi luxid"
+STOPDAEMONS_LIST="luxid rapi noded wconfd"
 
 if grep -q '^ENABLE_CONFD = True' lib/_constants.py; then
   DAEMONS_LIST="$DAEMONS_LIST confd"
