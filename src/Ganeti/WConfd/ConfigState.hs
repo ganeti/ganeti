d546d209248297b1d76e198fbdd9d4ca75147a1f
Petr Pudlak <pudlak@google.com>
2014-05-14 09:33:49 +0200

    Add a function for bumping the serial number to WConfd
    .. and also add a lens for 'csConfigData'.
    
    Signed-off-by: Petr Pudlak <pudlak@google.com>
    Reviewed-by: Klaus Aehlig <aehlig@google.com>

--- src/Ganeti/WConfd/ConfigState.hs
+++ src/Ganeti/WConfd/ConfigState.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 {-| Pure functions for manipulating the configuration state.
 
 -}
@@ -26,14 +28,19 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 module Ganeti.WConfd.ConfigState
   ( ConfigState
   , csConfigData
+  , csConfigDataL
   , mkConfigState
+  , bumpSerial
   , needsFullDist
   ) where
 
 import Control.Applicative
 import Data.Function (on)
+import System.Time (ClockTime(..))
 
+import Ganeti.Lens
 import Ganeti.Objects
+import Ganeti.Objects.Lens
 
 -- | In future this data type will include the current configuration
 -- ('ConfigData') and the last 'FStat' of its file.
@@ -42,10 +49,15 @@ data ConfigState = ConfigState
   }
   deriving (Eq, Show)
 
+$(makeCustomLenses ''ConfigState)
+
 -- | Creates a new configuration state.
 -- This method will expand as more fields are added to 'ConfigState'.
 mkConfigState :: ConfigData -> ConfigState
 mkConfigState = ConfigState
+
+bumpSerial :: (SerialNoObjectL a, TimeStampObjectL a) => ClockTime -> a -> a
+bumpSerial now = set mTimeL now . over serialL succ
 
 -- | Given two versions of the configuration, determine if its distribution
 -- needs to be fully commited before returning the corresponding call to
