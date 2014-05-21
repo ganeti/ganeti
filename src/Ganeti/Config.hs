68586d9ee452055d75b15879ceca6823e684fc7b
Petr Pudlak <pudlak@google.com>
2014-05-21 09:56:03 +0200

    Structure Ganeti.Config a bit
    .. by adding sections and moving functions into them.
    
    Signed-off-by: Petr Pudlak <pudlak@google.com>
    Reviewed-by: Klaus Aehlig <aehlig@google.com>

--- src/Ganeti/Config.hs
+++ src/Ganeti/Config.hs
@@ -81,9 +81,7 @@ import Ganeti.Types
 -- | Type alias for the link and ip map.
 type LinkIpMap = M.Map String (M.Map String String)
 
--- | Type class denoting objects which have node parameters.
-class NdParamObject a where
-  getNdParamsOf :: ConfigData -> a -> Maybe FilledNDParams
+-- * Operations on the whole configuration
 
 -- | Reads the config file.
 readConfig :: FilePath -> IO (Result String)
@@ -289,18 +287,6 @@ getGroupInstances cfg gname =
       ginsts = map (getNodeInstances cfg) gnodes in
   (concatMap fst ginsts, concatMap snd ginsts)
 
--- | Looks up a network. If looking up by uuid fails, we look up
--- by name.
-getNetwork :: ConfigData -> String -> ErrorResult Network
-getNetwork cfg name =
-  let networks = fromContainer (configNetworks cfg)
-  in case getItem "Network" name networks of
-       Ok net -> Ok net
-       Bad _ -> let by_name = M.mapKeys
-                              (fromNonEmpty . networkName . (M.!) networks)
-                              networks
-                in getItem "Network" name by_name
-
 -- | Retrieves the instance hypervisor params, missing values filled with
 -- cluster defaults.
 getFilledInstHvParams :: [String] -> ConfigData -> Instance -> HvParams
@@ -474,6 +460,26 @@ getNodeNdParams cfg node = do
   group <- getGroupOfNode cfg node
   let gparams = getGroupNdParams cfg group
   return $ fillNDParams gparams (nodeNdparams node)
+
+-- * Network
+
+-- | Looks up a network. If looking up by uuid fails, we look up
+-- by name.
+getNetwork :: ConfigData -> String -> ErrorResult Network
+getNetwork cfg name =
+  let networks = fromContainer (configNetworks cfg)
+  in case getItem "Network" name networks of
+       Ok net -> Ok net
+       Bad _ -> let by_name = M.mapKeys
+                              (fromNonEmpty . networkName . (M.!) networks)
+                              networks
+                in getItem "Network" name by_name
+
+-- * ND params
+
+-- | Type class denoting objects which have node parameters.
+class NdParamObject a where
+  getNdParamsOf :: ConfigData -> a -> Maybe FilledNDParams
 
 instance NdParamObject Node where
   getNdParamsOf = getNodeNdParams
