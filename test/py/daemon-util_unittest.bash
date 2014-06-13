fb76ff610abe1b618beb8551a3964fda7caff0d5
Petr Pudlak <pudlak@google.com>
2014-06-13 17:53:39 +0200

    Fix merge from 2.11: remove kvmd from on-demand daemons
    .. from yet another place.
    
    Signed-off-by: Petr Pudlak <pudlak@google.com>
    Reviewed-by: Klaus Aehlig <aehlig@google.com>

--- test/py/daemon-util_unittest.bash
+++ test/py/daemon-util_unittest.bash
@@ -49,7 +49,7 @@ if grep -q '^ENABLE_MOND = True' lib/_constants.py; then
   STOPDAEMONS_LIST="mond $STOPDAEMONS_LIST"
 fi
 
-STOPDAEMONS_LIST="kvmd metad $STOPDAEMONS_LIST"
+STOPDAEMONS_LIST="metad $STOPDAEMONS_LIST"
 
 DAEMONS=$(echo $(for d in $DAEMONS_LIST; do echo "ganeti-$d"; done))
 STOPDAEMONS=$(echo $(for d in $STOPDAEMONS_LIST; do echo "ganeti-$d"; done))
