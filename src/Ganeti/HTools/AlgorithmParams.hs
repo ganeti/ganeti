8e9dfe5cb71ca05eca9a871fc15ff039d71c2414
Klaus Aehlig <aehlig@google.com>
2014-07-02 17:35:39 +0200

    Add a module for htools' algorithm parameters
    Over time, the htools accumulated quite a few flags influencing
    the moves considered by htools. So far, all those flags are passed
    as positional arguments. This bares the risk of passing them in wrong
    order as type checking cannot distinguish different arguments of the
    same type (usually Bool). It also renders the function calls hard
    to read. So, add a module capsuling the parameters as named record
    fields.
    
    Signed-off-by: Klaus Aehlig <aehlig@google.com>
    Reviewed-by: Petr Pudlak <pudlak@google.com>

---
+++ src/Ganeti/HTools/AlgorithmParams.hs
@@ -0,0 +1,41 @@
+{-| Algorithm Options for HTools
+
+This module describes the parameters that influence the balancing
+algorithm in htools.
+
+-}
+
+{-
+
+Copyright (C) 2014 Google Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.
+
+-}
+
+module Ganeti.HTools.AlgorithmParams
+  ( AlgorithmOptions(..)
+  ) where
+
+data AlgorithmOptions = AlgorithmOptions
+  { algDiskMoves :: Bool            -- ^ Whether disk moves are allowed
+  , algInstanceMoves :: Bool        -- ^ Whether instance moves are allowed
+  , algRestrictedMigration :: Bool  -- ^ Whether migration is restricted
+  , algIgnoreSoftErrors :: Bool     -- ^ Whether to always ignore soft errors
+  , algEvacMode :: Bool             -- ^ Consider only eavacation moves
+  , algMinGain :: Double            -- ^ Minimal gain per balancing step
+  , algMinGainLimit :: Double       -- ^ Limit below which minimal gain is used
+  }
