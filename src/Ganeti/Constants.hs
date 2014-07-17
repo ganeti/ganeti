46d5c0038e73ab34018a1d34cb40b095531c34e1
Petr Pudlak <pudlak@google.com>
2014-07-17 13:34:49 +0200

    Make the metadata daemon optional during configuration
    In particular, skip its compilation, if the required packages aren't
    present.
    
    Signed-off-by: Petr Pudlak <pudlak@google.com>
    Reviewed-by: Klaus Aehlig <aehlig@google.com>

--- src/Ganeti/Constants.hs
+++ src/Ganeti/Constants.hs
@@ -323,6 +323,9 @@ enableConfd = AutoConf.enableConfd
 enableMond :: Bool
 enableMond = AutoConf.enableMond
 
+enableMetad :: Bool
+enableMetad = AutoConf.enableMetad
+
 enableRestrictedCommands :: Bool
 enableRestrictedCommands = AutoConf.enableRestrictedCommands
 
