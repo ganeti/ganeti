bbb7525271f525f436962c80b29372efaa303cad
Klaus Aehlig <aehlig@google.com>
2014-07-22 13:27:37 +0200

    LUInstanceGrowDisk needs to acquire locks sequentially
    Signed-off-by: Klaus Aehlig <aehlig@google.com>
    Reviewed-by: Petr Pudlak <pudlak@google.com>

--- lib/cmdlib/instance_storage.py
+++ lib/cmdlib/instance_storage.py
@@ -1560,6 +1560,8 @@ class LUInstanceGrowDisk(LogicalUnit):
     self.needed_locks[locking.LEVEL_NODE_RES] = []
     self.recalculate_locks[locking.LEVEL_NODE] = constants.LOCKS_REPLACE
     self.recalculate_locks[locking.LEVEL_NODE_RES] = constants.LOCKS_REPLACE
+    self.dont_collate_locks[locking.LEVEL_NODE] = True
+    self.dont_collate_locks[locking.LEVEL_NODE_RES] = True
 
   def DeclareLocks(self, level):
     if level == locking.LEVEL_NODE:
