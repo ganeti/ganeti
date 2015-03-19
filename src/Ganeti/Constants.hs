341d74ea584b1fce36731b5a450dc6f54d06ced6
Klaus Aehlig <aehlig@google.com>
2015-03-19 16:35:28 +0100

    Add a prefix for a WConfD livelock
    ...so that WConfD will be able to own locks for itself.
    This will be necessary on cluster destruction, where it
    takes over the BGL.
    
    Signed-off-by: Klaus Aehlig <aehlig@google.com>
    Reviewed-by: Petr Pudlak <pudlak@google.com>

--- src/Ganeti/Constants.hs
+++ src/Ganeti/Constants.hs
@@ -4126,6 +4126,10 @@ wconfdDefCtmo = 10
 wconfdDefRwto :: Int
 wconfdDefRwto = 60
 
+-- | The prefix of the WConfD livelock file name
+wconfdLivelockPrefix :: String
+wconfdLivelockPrefix = "wconf-daemon"
+
 -- * Confd
 
 confdProtocolVersion :: Int
