f5f941c8ef93caa447c759667cc0cca07891109b
Klaus Aehlig <aehlig@google.com>
2015-09-28 12:06:27 +0200

    Add a constant for a new node-setup paramter
    Signed-off-by: Klaus Aehlig <aehlig@google.com>
    Reviewed-by: Petr Pudlak <pudlak@google.com>
    (cherry picked from commit c99ad7bd9c84b134050ed5d0ae47b65708aa0fa6)

--- src/Ganeti/Constants.hs
+++ src/Ganeti/Constants.hs
@@ -4833,6 +4833,9 @@ ndsNodeDaemonCertificate = "node_daemon_certificate"
 ndsSsconf :: String
 ndsSsconf = "ssconf"
 
+ndsHmac :: String
+ndsHmac = "hmac_key"
+
 ndsStartNodeDaemon :: String
 ndsStartNodeDaemon = "start_node_daemon"
 
