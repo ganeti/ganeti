ganeti/ganeti

commit f3b51166d83c60bf7ee20da41aa6a3cfc285a518

Incapsulate RAPI authentication into a separate module
Functions and classes related to authentication moved to rapi.auth
module and are abstracted by RapiAuthenticator class in order to
simplify implementation of other authentication modules.

Signed-off-by: Oleg Ponomarev <oponomarev@google.com>
Reviewed-by: Klaus Aehlig <aehlig@google.com>

lib/http/auth.py
@@ -136,8 +136,8 @@ class HttpServerRequestAuthentication(object):
     if not realm:
       raise AssertionError("No authentication realm")
 
-    # Check "Authorization" header
-    if self._CheckAuthorization(req):
+    # Check Authentication
+    if self.Authenticate(req):
       # User successfully authenticated
       return
 
@@ -155,24 +155,25 @@ class HttpServerRequestAuthentication(object):
 
     raise http.HttpUnauthorized(headers=headers)
 
-  def _CheckAuthorization(self, req):
-    """Checks 'Authorization' header sent by client.
+  @classmethod
+  def ExtractUserPassword(cls, req):
+    """Extracts a user and a password from the http authorization header.
 
     @type req: L{http.server._HttpServerRequest}
-    @param req: HTTP request context
-    @rtype: bool
-    @return: Whether user is allowed to execute request
-
+    @param req: HTTP request
+    @rtype: (str, str)
+    @return: A tuple containing a user and a password. One or both values
+             might be None if they are not presented
     """
     credentials = req.request_headers.get(http.HTTP_AUTHORIZATION, None)
     if not credentials:
-      return False
+      return None, None
 
     # Extract scheme
     parts = credentials.strip().split(None, 2)
     if len(parts) < 1:
       # Missing scheme
-      return False
+      return None, None
 
     # RFC2617, section 1.2: "[...] It uses an extensible, case-insensitive
     # token to identify the authentication scheme [...]"
@@ -183,7 +184,7 @@ class HttpServerRequestAuthentication(object):
       if len(parts) < 2:
         raise http.HttpBadRequest(message=("Basic authentication requires"
                                            " credentials"))
-      return self._CheckBasicAuthorization(req, parts[1])
+      return cls._ExtractBasicUserPassword(parts[1])
 
     elif scheme == HTTP_DIGEST_AUTH.lower():
       # TODO: Implement digest authentication
@@ -193,39 +194,72 @@ class HttpServerRequestAuthentication(object):
       pass
 
     # Unsupported authentication scheme
-    return False
+    return None, None
 
-  def _CheckBasicAuthorization(self, req, in_data):
-    """Checks credentials sent for basic authentication.
+  @staticmethod
+  def _ExtractBasicUserPassword(in_data):
+    """Extracts user and password from the contents of an authorization header.
 
-    @type req: L{http.server._HttpServerRequest}
-    @param req: HTTP request context
     @type in_data: str
     @param in_data: Username and password encoded as Base64
-    @rtype: bool
-    @return: Whether user is allowed to execute request
+    @rtype: (str, str)
+    @return: A tuple containing user and password. One or both values might be
+             None if they are not presented
 
     """
     try:
       creds = base64.b64decode(in_data.encode("ascii")).decode("ascii")
     except (TypeError, binascii.Error, UnicodeError):
       logging.exception("Error when decoding Basic authentication credentials")
-      return False
+      raise http.HttpBadRequest(message=("Invalid basic authorization header"))
 
     if ":" not in creds:
-      return False
+      # We have just a username without password
+      return creds, None
 
-    (user, password) = creds.split(":", 1)
+    # return (user, password) tuple
+    return creds.split(":", 1)
 
-    return self.Authenticate(req, user, password)
-
-  def Authenticate(self, req, user, password):
-    """Checks the password for a user.
+  def Authenticate(self, req):
+    """Checks the credentiales.
 
     This function MUST be overridden by a subclass.
 
     """
     raise NotImplementedError()
+
+  @classmethod
+  def ExtractSchemePassword(cls, expected_password):
+    """Extracts a scheme and a password from the expected_password.
+
+    @type expected_password: str
+    @param expected_password: Username and password encoded as Base64
+    @rtype: (str, str)
+    @return: A tuple containing a scheme and a password. Both values will be
+             None when an invalid scheme or password encoded
+
+    """
+    if expected_password is None:
+      return None, None
+    # Backwards compatibility for old-style passwords without a scheme
+    if not expected_password.startswith("{"):
+      expected_password = cls._CLEARTEXT_SCHEME + expected_password
+
+    # Check again, just to be sure
+    if not expected_password.startswith("{"):
+      raise AssertionError("Invalid scheme")
+
+    scheme_end_idx = expected_password.find("}", 1)
+
+    # Ensure scheme has a length of at least one character
+    if scheme_end_idx <= 1:
+      logging.warning("Invalid scheme in password")
+      return None, None
+
+    scheme = expected_password[:scheme_end_idx + 1].upper()
+    password = expected_password[scheme_end_idx + 1:]
+
+    return scheme, password
 
   def VerifyBasicAuthPassword(self, req, username, password, expected):
     """Checks the password for basic authentication.
@@ -245,23 +279,10 @@ class HttpServerRequestAuthentication(object):
                      users file)
 
     """
-    # Backwards compatibility for old-style passwords without a scheme
-    if not expected.startswith("{"):
-      expected = self._CLEARTEXT_SCHEME + expected
 
-    # Check again, just to be sure
-    if not expected.startswith("{"):
-      raise AssertionError("Invalid scheme")
-
-    scheme_end_idx = expected.find("}", 1)
-
-    # Ensure scheme has a length of at least one character
-    if scheme_end_idx <= 1:
-      logging.warning("Invalid scheme in password for user '%s'", username)
+    scheme, expected_password = self.ExtractSchemePassword(expected)
+    if scheme is None or password is None:
       return False
-
-    scheme = expected[:scheme_end_idx + 1].upper()
-    expected_password = expected[scheme_end_idx + 1:]
 
     # Good old plain text password
     if scheme == self._CLEARTEXT_SCHEME:
@@ -283,4 +304,3 @@ class HttpServerRequestAuthentication(object):
                     scheme, username)
 
     return False
-

lib/rapi/auth/__init__.py
@@ -1,0 +1,52 @@
+#
+#
+
+# Copyright (C) 2015 Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""Module containing different authentificators that can be used by RAPI
+
+"""
+
+
+class RapiAuthenticator(object):
+  """Class providing authentication interface for RAPi requests.
+
+  """
+  def ValidateRequest(self, req, handler_access):
+    """Checks whether it's permitted to execute an rapi request.
+
+    Must be implemented in derived classes.
+
+    @type req: L{http.server._HttpServerRequest}
+    @param req: HTTP request context
+    @type handler_access: set of strings
+    @param handler_access: access rights required by the requested resourse
+    @rtype: bool
+    @return: Whether request execution is permitted
+
+    """
+    raise NotImplementedError()

lib/rapi/auth/basic_auth.py
@@ -1,0 +1,148 @@
+#
+#
+
+# Copyright (C) 2006, 2007, 2008, 2009, 2010, 2012, 2013, 2015 Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+"""Module interacting with RAPI users config file
+
+"""
+
+import logging
+import os
+try:
+  from pyinotify import pyinotify # pylint: disable=E0611
+except ImportError:
+  import pyinotify
+
+from ganeti import asyncnotifier
+from ganeti import compat
+from ganeti import http
+from ganeti.http.auth import HttpServerRequestAuthentication
+from ganeti import pathutils
+from ganeti.rapi import auth
+from ganeti.rapi.auth import users_file
+
+
+class FileEventHandler(asyncnotifier.FileEventHandlerBase):
+  def __init__(self, wm, path, cb):
+    """Initializes this class.
+
+    @param wm: Inotify watch manager
+    @type path: string
+    @param path: File path
+    @type cb: callable
+    @param cb: Function called on file change
+
+    """
+    asyncnotifier.FileEventHandlerBase.__init__(self, wm)
+
+    self._cb = cb
+    self._filename = os.path.basename(path)
+
+    # Different Pyinotify versions have the flag constants at different places,
+    # hence not accessing them directly
+    mask = (pyinotify.EventsCodes.ALL_FLAGS["IN_CLOSE_WRITE"] |
+            pyinotify.EventsCodes.ALL_FLAGS["IN_DELETE"] |
+            pyinotify.EventsCodes.ALL_FLAGS["IN_MOVED_FROM"] |
+            pyinotify.EventsCodes.ALL_FLAGS["IN_MOVED_TO"])
+
+    self._handle = self.AddWatch(os.path.dirname(path), mask)
+
+  def process_default(self, event):
+    """Called upon inotify event.
+
+    """
+    if event.name == self._filename:
+      logging.debug("Received inotify event %s", event)
+      self._cb()
+
+
+def SetupFileWatcher(filename, cb):
+  """Configures an inotify watcher for a file.
+
+  @type filename: string
+  @param filename: File to watch
+  @type cb: callable
+  @param cb: Function called on file change
+
+  """
+  wm = pyinotify.WatchManager()
+  handler = FileEventHandler(wm, filename, cb)
+  asyncnotifier.AsyncNotifier(wm, default_proc_fun=handler)
+
+
+class BasicAuthenticator(auth.RapiAuthenticator):
+  """Class providing an Authenticate method based on basic http authentication.
+
+  """
+
+  def __init__(self, user_fn=None):
+    """Loads users file and initializes a watcher for it.
+
+    @param user_fn: A function that should be called to obtain a user info
+                    instead of the default users_file interface.
+
+    """
+    self.user_fn = user_fn
+    if user_fn:
+      return
+
+    self.users = users_file.RapiUsers()
+    # Setup file watcher (it'll be driven by asyncore)
+    SetupFileWatcher(pathutils.RAPI_USERS_FILE,
+                     compat.partial(self.users.Load,
+                                    pathutils.RAPI_USERS_FILE))
+
+    self.users.Load(pathutils.RAPI_USERS_FILE)
+
+  def ValidateRequest(self, req, handler_access):
+    """Checks whether a user can access a resource.
+
+    """
+    username, password = HttpServerRequestAuthentication \
+                           .ExtractUserPassword(req)
+    if username is None:
+      raise http.HttpUnauthorized()
+    if password is None:
+      raise http.HttpBadRequest(message=("Basic authentication requires"
+                                           " password"))
+
+    user = self.user_fn(username) if self.user_fn else self.users.Get(username)
+    _, expected_password = HttpServerRequestAuthentication \
+                             .ExtractSchemePassword(password)
+    if not (user and expected_password == user.password):
+      # Unknown user or password wrong
+      return False
+
+    if (not handler_access or
+        set(user.options).intersection(handler_access)):
+      # Allow access
+      return True
+
+    # Access forbidden
+    raise http.HttpForbidden()

lib/rapi/testutils.py
@@ -50,9 +50,8 @@ from ganeti import rapi
 
 import ganeti.http.server # pylint: disable=W0611
 import ganeti.server.rapi
-from ganeti.rapi import users_file
+from ganeti.rapi.auth import users_file
 import ganeti.rapi.client
-
 
 _URI_RE = re.compile(r"https://(?P<host>.*):(?P<port>\d+)(?P<path>/.*)")
 
@@ -359,18 +358,23 @@ class InputTestClient(object):
     username = utils.GenerateSecret()
     password = utils.GenerateSecret()
 
-    def user_fn(wanted):
-      """Called to verify user credentials given in HTTP request.
+    # pylint: disable=W0232
+    class SimpleAuthenticator():
+      # pylint: disable=R0201
+      def ValidateRequest(self, req, _=None):
+        """Called to verify user credentials given in HTTP request.
 
-      """
-      assert username == wanted
-      return users_file.PasswordFileUser(username, password,
-                                         [rapi.RAPI_ACCESS_WRITE])
+        """
+        wanted, _ = http.auth.HttpServerRequestAuthentication \
+                      .ExtractUserPassword(req)
+        assert username == wanted
+        return users_file.PasswordFileUser(username, password,
+                                           [rapi.RAPI_ACCESS_WRITE]).name
 
     self._lcr = _LuxiCallRecorder()
 
     # Create a mock RAPI server
-    handler = _RapiMock(user_fn, self._lcr)
+    handler = _RapiMock(SimpleAuthenticator(), self._lcr)
 
     self._client = \
       rapi.client.GanetiRapiClient("master.example.com",

commit 48b78207955da5b6cead10e96ecadd6c5c061cea

Fix RAPI basic authentication
RAPI basic authentication was broken in commit
f3b51166d83c60bf7ee20da41aa6a3cfc285a518.

Signed-off-by: Nicolas Avrutin <rasputin@google.com>
Signed-off-by: Klaus Aehlig <aehlig@google.com>
Reviewed-by: Klaus Aehlig <aehlig@google.com>

lib/http/auth.py
@@ -155,8 +155,8 @@ class HttpServerRequestAuthentication(object):
 
     raise http.HttpUnauthorized(headers=headers)
 
-  @classmethod
-  def ExtractUserPassword(cls, req):
+  @staticmethod
+  def ExtractUserPassword(req):
     """Extracts a user and a password from the http authorization header.
 
     @type req: L{http.server._HttpServerRequest}
@@ -184,7 +184,7 @@ class HttpServerRequestAuthentication(object):
       if len(parts) < 2:
         raise http.HttpBadRequest(message=("Basic authentication requires"
                                            " credentials"))
-      return cls._ExtractBasicUserPassword(parts[1])
+      return HttpServerRequestAuthentication._ExtractBasicUserPassword(parts[1])
 
     elif scheme == HTTP_DIGEST_AUTH.lower():
       # TODO: Implement digest authentication
@@ -228,8 +228,8 @@ class HttpServerRequestAuthentication(object):
     """
     raise NotImplementedError()
 
-  @classmethod
-  def ExtractSchemePassword(cls, expected_password):
+  @staticmethod
+  def ExtractSchemePassword(expected_password):
     """Extracts a scheme and a password from the expected_password.
 
     @type expected_password: str
@@ -243,7 +243,8 @@ class HttpServerRequestAuthentication(object):
       return None, None
     # Backwards compatibility for old-style passwords without a scheme
     if not expected_password.startswith("{"):
-      expected_password = cls._CLEARTEXT_SCHEME + expected_password
+      expected_password = (HttpServerRequestAuthentication._CLEARTEXT_SCHEME +
+                           expected_password)
 
     # Check again, just to be sure
     if not expected_password.startswith("{"):
@@ -261,15 +262,14 @@ class HttpServerRequestAuthentication(object):
 
     return scheme, password
 
-  def VerifyBasicAuthPassword(self, req, username, password, expected):
+  @staticmethod
+  def VerifyBasicAuthPassword(username, password, expected, realm):
     """Checks the password for basic authentication.
 
     As long as they don't start with an opening brace ("E{lb}"), old passwords
     are supported. A new scheme uses H(A1) from RFC2617, where H is MD5 and A1
     consists of the username, the authentication realm and the actual password.
 
-    @type req: L{http.server._HttpServerRequest}
-    @param req: HTTP request context
     @type username: string
     @param username: Username from HTTP headers
     @type password: string
@@ -277,20 +277,21 @@ class HttpServerRequestAuthentication(object):
     @type expected: string
     @param expected: Expected password with optional scheme prefix (e.g. from
                      users file)
+    @type realm: string
+    @param realm: Authentication realm
 
     """
-
-    scheme, expected_password = self.ExtractSchemePassword(expected)
+    scheme, expected_password = HttpServerRequestAuthentication \
+                                  .ExtractSchemePassword(expected)
     if scheme is None or password is None:
       return False
 
     # Good old plain text password
-    if scheme == self._CLEARTEXT_SCHEME:
+    if scheme == HttpServerRequestAuthentication._CLEARTEXT_SCHEME:
       return password == expected_password
 
     # H(A1) as described in RFC2617
-    if scheme == self._HA1_SCHEME:
-      realm = self.GetAuthRealm(req)
+    if scheme == HttpServerRequestAuthentication._HA1_SCHEME:
       if not realm:
         # There can not be a valid password for this case
         raise AssertionError("No authentication realm")

lib/rapi/auth/__init__.py
@@ -36,7 +36,7 @@ class RapiAuthenticator(object):
   """Class providing authentication interface for RAPi requests.
 
   """
-  def ValidateRequest(self, req, handler_access):
+  def ValidateRequest(self, req, handler_access, realm):
     """Checks whether it's permitted to execute an rapi request.
 
     Must be implemented in derived classes.
@@ -45,6 +45,8 @@ class RapiAuthenticator(object):
     @param req: HTTP request context
     @type handler_access: set of strings
     @param handler_access: access rights required by the requested resourse
+    @type realm: string
+    @param realm: Authentication realm
     @rtype: str or None
     @return: the authenticated user name if request execution is permitted and
              None otherwise

lib/rapi/auth/basic_auth.py
@@ -108,11 +108,12 @@ class BasicAuthenticator(auth.RapiAuthenticator):
                     instead of the default users_file interface.
 
     """
-    self.user_fn = user_fn
     if user_fn:
+      self.user_fn = user_fn
       return
 
     self.users = users_file.RapiUsers()
+    self.user_fn = self.users.Get
     # Setup file watcher (it'll be driven by asyncore)
     SetupFileWatcher(pathutils.RAPI_USERS_FILE,
                      compat.partial(self.users.Load,
@@ -120,29 +121,30 @@ class BasicAuthenticator(auth.RapiAuthenticator):
 
     self.users.Load(pathutils.RAPI_USERS_FILE)
 
-  def ValidateRequest(self, req, handler_access):
+  def ValidateRequest(self, req, handler_access, realm):
     """Checks whether a user can access a resource.
 
     """
-    username, password = HttpServerRequestAuthentication \
-                           .ExtractUserPassword(req)
-    if username is None:
+    request_username, request_password = HttpServerRequestAuthentication \
+                                           .ExtractUserPassword(req)
+    if request_username is None:
       raise http.HttpUnauthorized()
-    if password is None:
+    if request_password is None:
       raise http.HttpBadRequest(message=("Basic authentication requires"
-                                           " password"))
+                                         " password"))
 
-    user = self.user_fn(username) if self.user_fn else self.users.Get(username)
-    _, expected_password = HttpServerRequestAuthentication \
-                             .ExtractSchemePassword(password)
-    if not (user and expected_password == user.password):
+    user = self.user_fn(request_username)
+    if not (user and HttpServerRequestAuthentication
+                       .VerifyBasicAuthPassword(request_username,
+                                                request_password,
+                                                user.password, realm)):
       # Unknown user or password wrong
       return None
 
     if (not handler_access or
         set(user.options).intersection(handler_access)):
       # Allow access
-      return username
+      return request_username
 
     # Access forbidden
     raise http.HttpForbidden()

lib/rapi/auth/pam.py
@@ -359,7 +359,7 @@ class PamAuthenticator(auth.RapiAuthenticator):
     """
     self.cf = CFunctions()
 
-  def ValidateRequest(self, req, handler_access):
+  def ValidateRequest(self, req, handler_access, _):
     """Checks whether a user can access a resource.
 
     This function retuns authenticated user name on success.

lib/rapi/testutils.py
@@ -361,7 +361,7 @@ class InputTestClient(object):
     # pylint: disable=W0232
     class SimpleAuthenticator():
       # pylint: disable=R0201
-      def ValidateRequest(self, req, _=None):
+      def ValidateRequest(self, req, _handler_access, _realm):
         """Called to verify user credentials given in HTTP request.
 
         """

lib/server/rapi.py
@@ -154,7 +154,8 @@ class RemoteApiHandler(http.auth.HttpServerRequestAuthentication,
     @return: username of an authenticated user or None otherwise
     """
     ctx = self._GetRequestContext(req)
-    auth_user = self._authenticator.ValidateRequest(req, ctx.handler_access)
+    auth_user = self._authenticator.ValidateRequest(
+        req, ctx.handler_access, self.GetAuthRealm(req))
     if auth_user is None:
       return False
 

test/py/ganeti.http_unittest.py
@@ -135,9 +135,11 @@ class _FakeRequestAuth(http.auth.HttpServerRequestAuthentication):
   def GetAuthRealm(self, req):
     return self.realm
 
-  def Authenticate(self, *args):
+  def Authenticate(self, req):
+    handler_access = []
     if self.authenticator:
-      return self.authenticator.ValidateRequest(*args)
+      return self.authenticator.ValidateRequest(
+          req, handler_access, self.GetAuthRealm(req))
     raise NotImplementedError()
 
 
@@ -155,7 +157,7 @@ class TestAuth(unittest.TestCase):
   def _testVerifyBasicAuthPassword(self, realm, user, password, expected):
     ra = _FakeRequestAuth(realm, False, None)
 
-    return ra.VerifyBasicAuthPassword(None, user, password, expected)
+    return ra.VerifyBasicAuthPassword(user, password, expected, realm)
 
   def testVerifyBasicAuthPassword(self):
     tvbap = self._testVerifyBasicAuthPassword
@@ -204,7 +206,7 @@ class _SimpleAuthenticator:
     self.password = password
     self.called = False
 
-  def ValidateRequest(self, req, _=None):
+  def ValidateRequest(self, req, handler_access, realm):
     self.called = True
 
     username, password = http.auth.HttpServerRequestAuthentication \
@@ -212,12 +214,9 @@ class _SimpleAuthenticator:
     if username is None or password is None:
       return False
 
-    _, expected_password = http.auth.HttpServerRequestAuthentication \
-                             .ExtractSchemePassword(password)
-    if self.user == username and expected_password == self.password:
-      return True
-
-    return False
+    return (self.user == username and
+            http.auth.HttpServerRequestAuthentication.VerifyBasicAuthPassword(
+                username, password, self.password, realm))
 
 
 class TestHttpServerRequestAuthentication(unittest.TestCase):