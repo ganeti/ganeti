ganeti/ganeti

commit f3b51166d83c60bf7ee20da41aa6a3cfc285a518

Incapsulate RAPI authentication into a separate module
Functions and classes related to authentication moved to rapi.auth
module and are abstracted by RapiAuthenticator class in order to
simplify implementation of other authentication modules.

Signed-off-by: Oleg Ponomarev <oponomarev@google.com>
Reviewed-by: Klaus Aehlig <aehlig@google.com>

lib/server/rapi.py
@@ -38,26 +38,17 @@
 import logging
 import optparse
 import sys
-import os
-import os.path
 
-try:
-  from pyinotify import pyinotify # pylint: disable=E0611
-except ImportError:
-  import pyinotify
-
-from ganeti import asyncnotifier
 from ganeti import constants
 from ganeti import http
 from ganeti import daemon
 from ganeti import ssconf
 import ganeti.rpc.errors as rpcerr
 from ganeti import serializer
-from ganeti import compat
 from ganeti import pathutils
 from ganeti.rapi import connector
 from ganeti.rapi import baserlib
-from ganeti.rapi import users_file
+from ganeti.rapi.auth import basic_auth
 
 import ganeti.http.auth   # pylint: disable=W0611
 import ganeti.http.server
@@ -81,12 +72,12 @@ class RemoteApiHandler(http.auth.HttpServerRequestAuthentication,
   """
   AUTH_REALM = "Ganeti Remote API"
 
-  def __init__(self, user_fn, reqauth, _client_cls=None):
+  def __init__(self, authenticator, reqauth, _client_cls=None):
     """Initializes this class.
 
-    @type user_fn: callable
-    @param user_fn: Function receiving username as string and returning
-      L{http.auth.PasswordFileUser} or C{None} if user is not found
+    @type authenticator: an implementation of {RapiAuthenticator} interface
+    @param authenticator: a class containing an implementation of
+                          ValidateRequest function
     @type reqauth: bool
     @param reqauth: Whether to require authentication
 
@@ -97,7 +88,7 @@ class RemoteApiHandler(http.auth.HttpServerRequestAuthentication,
     http.auth.HttpServerRequestAuthentication.__init__(self)
     self._client_cls = _client_cls
     self._resmap = connector.Mapper()
-    self._user_fn = user_fn
+    self._authenticator = authenticator
     self._reqauth = reqauth
 
   @staticmethod
@@ -154,28 +145,14 @@ class RemoteApiHandler(http.auth.HttpServerRequestAuthentication,
     """Determine whether authentication is required.
 
     """
-    return self._reqauth or bool(self._GetRequestContext(req).handler_access)
+    return self._reqauth
 
-  def Authenticate(self, req, username, password):
+  def Authenticate(self, req):
     """Checks whether a user can access a resource.
 
     """
     ctx = self._GetRequestContext(req)
-
-    user = self._user_fn(username)
-    if not (user and
-            self.VerifyBasicAuthPassword(req, username, password,
-                                         user.password)):
-      # Unknown user or password wrong
-      return False
-
-    if (not ctx.handler_access or
-        set(user.options).intersection(ctx.handler_access)):
-      # Allow access
-      return True
-
-    # Access forbidden
-    raise http.HttpForbidden()
+    return self._authenticator.ValidateRequest(req, ctx.handler_access)
 
   def HandleRequest(self, req):
     """Handles a request.
@@ -213,54 +190,6 @@ class RemoteApiHandler(http.auth.HttpServerRequestAuthentication,
     return serializer.DumpJson(result)
 
 
-class FileEventHandler(asyncnotifier.FileEventHandlerBase):
-  def __init__(self, wm, path, cb):
-    """Initializes this class.
-
-    @param wm: Inotify watch manager
-    @type path: string
-    @param path: File path
-    @type cb: callable
-    @param cb: Function called on file change
-
-    """
-    asyncnotifier.FileEventHandlerBase.__init__(self, wm)
-
-    self._cb = cb
-    self._filename = os.path.basename(path)
-
-    # Different Pyinotify versions have the flag constants at different places,
-    # hence not accessing them directly
-    mask = (pyinotify.EventsCodes.ALL_FLAGS["IN_CLOSE_WRITE"] |
-            pyinotify.EventsCodes.ALL_FLAGS["IN_DELETE"] |
-            pyinotify.EventsCodes.ALL_FLAGS["IN_MOVED_FROM"] |
-            pyinotify.EventsCodes.ALL_FLAGS["IN_MOVED_TO"])
-
-    self._handle = self.AddWatch(os.path.dirname(path), mask)
-
-  def process_default(self, event):
-    """Called upon inotify event.
-
-    """
-    if event.name == self._filename:
-      logging.debug("Received inotify event %s", event)
-      self._cb()
-
-
-def SetupFileWatcher(filename, cb):
-  """Configures an inotify watcher for a file.
-
-  @type filename: string
-  @param filename: File to watch
-  @type cb: callable
-  @param cb: Function called on file change
-
-  """
-  wm = pyinotify.WatchManager()
-  handler = FileEventHandler(wm, filename, cb)
-  asyncnotifier.AsyncNotifier(wm, default_proc_fun=handler)
-
-
 def CheckRapi(options, args):
   """Initial checks whether to run or exit with a failure.
 
@@ -286,15 +215,9 @@ def PrepRapi(options, _):
   """
   mainloop = daemon.Mainloop()
 
-  users = users_file.RapiUsers()
+  authenticator = basic_auth.BasicAuthenticator()
 
-  handler = RemoteApiHandler(users.Get, options.reqauth)
-
-  # Setup file watcher (it'll be driven by asyncore)
-  SetupFileWatcher(pathutils.RAPI_USERS_FILE,
-                   compat.partial(users.Load, pathutils.RAPI_USERS_FILE))
-
-  users.Load(pathutils.RAPI_USERS_FILE)
+  handler = RemoteApiHandler(authenticator, options.reqauth)
 
   server = \
     http.server.HttpServer(mainloop, options.bind_address, options.port,
@@ -309,10 +232,12 @@ def ExecRapi(options, args, prep_data): # pylint: disable=W0613
   """Main remote API function, executed with the PID file held.
 
   """
+
   (mainloop, server) = prep_data
   try:
     mainloop.Run()
   finally:
+    logging.error("RAPI Daemon Failed")
     server.Stop()
 
 

test/py/ganeti.server.rapi_unittest.py
@@ -46,8 +46,8 @@ from ganeti import rapi
 from ganeti import http
 from ganeti import objects
 
-import ganeti.rapi.baserlib
-from ganeti.rapi import users_file
+from ganeti.rapi.auth.basic_auth import BasicAuthenticator
+from ganeti.rapi.auth import users_file
 import ganeti.rapi.testutils
 import ganeti.rapi.rlib2
 import ganeti.http.auth
@@ -63,7 +63,8 @@ class TestRemoteApiHandler(unittest.TestCase):
   def _Test(self, method, path, headers, reqbody,
             user_fn=NotImplemented, luxi_client=NotImplemented,
             reqauth=False):
-    rm = rapi.testutils._RapiMock(user_fn, luxi_client, reqauth=reqauth)
+    rm = rapi.testutils._RapiMock(BasicAuthenticator(user_fn), luxi_client,
+                                  reqauth=reqauth)
 
     (resp_code, resp_headers, resp_body) = \
       rm.FetchResponse(path, method, http.ParseHeaders(StringIO(headers)),
@@ -106,7 +107,8 @@ class TestRemoteApiHandler(unittest.TestCase):
     self.assertTrue(data["message"].startswith("Method PUT is unsupported"))
 
   def testPostInstancesNoAuth(self):
-    (code, _, _) = self._Test(http.HTTP_POST, "/2/instances", "", None)
+    (code, _, _) = self._Test(http.HTTP_POST, "/2/instances", "", None,
+                              reqauth=True)
     self.assertEqual(code, http.HttpUnauthorized.code)
 
   def testRequestWithUnsupportedMediaType(self):
@@ -135,7 +137,8 @@ class TestRemoteApiHandler(unittest.TestCase):
       "%s: %s" % (http.HTTP_AUTHORIZATION, "Unsupported scheme"),
       ])
 
-    (code, _, _) = self._Test(http.HTTP_POST, "/2/instances", headers, "")
+    (code, _, _) = self._Test(http.HTTP_POST, "/2/instances", headers, "",
+                              reqauth=True)
     self.assertEqual(code, http.HttpUnauthorized.code)
 
   def testIncompleteBasicAuth(self):
@@ -143,7 +146,8 @@ class TestRemoteApiHandler(unittest.TestCase):
       "%s: Basic" % http.HTTP_AUTHORIZATION,
       ])
 
-    (code, _, data) = self._Test(http.HTTP_POST, "/2/instances", headers, "")
+    (code, _, data) = self._Test(http.HTTP_POST, "/2/instances", headers, "",
+                                 reqauth=True)
     self.assertEqual(code, http.HttpBadRequest.code)
     self.assertEqual(data["message"],
                      "Basic authentication requires credentials")
@@ -155,8 +159,9 @@ class TestRemoteApiHandler(unittest.TestCase):
         "%s: Basic %s" % (http.HTTP_AUTHORIZATION, auth),
         ])
 
-      (code, _, data) = self._Test(http.HTTP_POST, "/2/instances", headers, "")
-      self.assertEqual(code, http.HttpUnauthorized.code)
+      (code, _, data) = self._Test(http.HTTP_POST, "/2/instances", headers, "",
+                                   reqauth=True)
+      self.assertEqual(code, http.HttpBadRequest.code)
 
   @staticmethod
   def _MakeAuthHeaders(username, password, correct_password):
@@ -197,7 +202,7 @@ class TestRemoteApiHandler(unittest.TestCase):
 
         for method in rapi.baserlib._SUPPORTED_METHODS:
           # No authorization
-          (code, _, _) = self._Test(method, path, "", "")
+          (code, _, _) = self._Test(method, path, "", "", reqauth=True)
 
           if method in (http.HTTP_DELETE, http.HTTP_POST):
             self.assertEqual(code, http.HttpNotImplemented.code)
@@ -207,22 +212,22 @@ class TestRemoteApiHandler(unittest.TestCase):
 
           # Incorrect user
           (code, _, _) = self._Test(method, path, header_fn(True), "",
-                                    user_fn=self._LookupWrongUser)
+                                    user_fn=self._LookupWrongUser, reqauth=True)
           self.assertEqual(code, http.HttpUnauthorized.code)
 
           # User has no write access, but the password is correct
           (code, _, _) = self._Test(method, path, header_fn(True), "",
-                                    user_fn=_LookupUserNoWrite)
+                                    user_fn=_LookupUserNoWrite, reqauth=True)
           self.assertEqual(code, http.HttpForbidden.code)
 
           # Wrong password and no write access
           (code, _, _) = self._Test(method, path, header_fn(False), "",
-                                    user_fn=_LookupUserNoWrite)
+                                    user_fn=_LookupUserNoWrite, reqauth=True)
           self.assertEqual(code, http.HttpUnauthorized.code)
 
           # Wrong password with write access
           (code, _, _) = self._Test(method, path, header_fn(False), "",
-                                    user_fn=_LookupUserWithWrite)
+                                    user_fn=_LookupUserWithWrite, reqauth=True)
           self.assertEqual(code, http.HttpUnauthorized.code)
 
           # Prepare request information
@@ -240,7 +245,8 @@ class TestRemoteApiHandler(unittest.TestCase):
           # User has write access, password is correct
           (code, _, data) = self._Test(method, reqpath, header_fn(True), body,
                                        user_fn=_LookupUserWithWrite,
-                                       luxi_client=_FakeLuxiClientForQuery)
+                                       luxi_client=_FakeLuxiClientForQuery,
+                                       reqauth=True)
           self.assertEqual(code, http.HTTP_OK)
           self.assertTrue(objects.QueryResponse.FromDict(data))
 
@@ -249,10 +255,14 @@ class TestRemoteApiHandler(unittest.TestCase):
 
     for method in rapi.baserlib._SUPPORTED_METHODS:
       for reqauth in [False, True]:
+        if method == http.HTTP_GET and not reqauth:
+          # we don't have a mock client to test this case
+          continue
         # No authorization
-        (code, _, _) = self._Test(method, path, "", "", reqauth=reqauth)
+        (code, _, _) = self._Test(method, path, "", "",
+                                  user_fn=lambda _ : None, reqauth=reqauth)
 
-        if method == http.HTTP_GET or reqauth:
+        if method == http.HTTP_GET and reqauth:
           self.assertEqual(code, http.HttpUnauthorized.code)
         else:
           self.assertEqual(code, http.HttpNotImplemented.code)

Makefile.am
@@ -99,6 +99,7 @@ httpdir = $(pkgpythondir)/http
 masterddir = $(pkgpythondir)/masterd
 confddir = $(pkgpythondir)/confd
 rapidir = $(pkgpythondir)/rapi
+rapi_authdir = $(pkgpythondir)/rapi/auth
 rpcdir = $(pkgpythondir)/rpc
 rpc_stubdir = $(pkgpythondir)/rpc/stub
 serverdir = $(pkgpythondir)/server
@@ -219,6 +220,7 @@ DIRS = \
 	lib/impexpd \
 	lib/masterd \
 	lib/rapi \
+	lib/rapi/auth \
 	lib/rpc \
 	lib/rpc/stub \
 	lib/server \
@@ -568,8 +570,12 @@ rapi_PYTHON = \
 	lib/rapi/client_utils.py \
 	lib/rapi/connector.py \
 	lib/rapi/rlib2.py \
-	lib/rapi/testutils.py \
-	lib/rapi/users_file.py
+	lib/rapi/testutils.py
+
+rapi_auth_PYTHON = \
+	lib/rapi/auth/__init__.py \
+	lib/rapi/auth/basic_auth.py \
+	lib/rapi/auth/users_file.py
 
 http_PYTHON = \
 	lib/http/__init__.py \
@@ -2121,6 +2127,7 @@ all_python_code = \
 	$(jqueue_PYTHON) \
 	$(storage_PYTHON) \
 	$(rapi_PYTHON) \
+	$(rapi_auth_PYTHON) \
 	$(server_PYTHON) \
 	$(rpc_PYTHON) \
 	$(rpc_stub_PYTHON) \

lib/http/auth.py
@@ -136,8 +136,8 @@ class HttpServerRequestAuthentication(object):
     if not realm:
       raise AssertionError("No authentication realm")
 
-    # Check "Authorization" header
-    if self._CheckAuthorization(req):
+    # Check Authentication
+    if self.Authenticate(req):
       # User successfully authenticated
       return
 
@@ -155,24 +155,25 @@ class HttpServerRequestAuthentication(object):
 
     raise http.HttpUnauthorized(headers=headers)
 
-  def _CheckAuthorization(self, req):
-    """Checks 'Authorization' header sent by client.
+  @classmethod
+  def ExtractUserPassword(cls, req):
+    """Extracts a user and a password from the http authorization header.
 
     @type req: L{http.server._HttpServerRequest}
-    @param req: HTTP request context
-    @rtype: bool
-    @return: Whether user is allowed to execute request
-
+    @param req: HTTP request
+    @rtype: (str, str)
+    @return: A tuple containing a user and a password. One or both values
+             might be None if they are not presented
     """
     credentials = req.request_headers.get(http.HTTP_AUTHORIZATION, None)
     if not credentials:
-      return False
+      return None, None
 
     # Extract scheme
     parts = credentials.strip().split(None, 2)
     if len(parts) < 1:
       # Missing scheme
-      return False
+      return None, None
 
     # RFC2617, section 1.2: "[...] It uses an extensible, case-insensitive
     # token to identify the authentication scheme [...]"
@@ -183,7 +184,7 @@ class HttpServerRequestAuthentication(object):
       if len(parts) < 2:
         raise http.HttpBadRequest(message=("Basic authentication requires"
                                            " credentials"))
-      return self._CheckBasicAuthorization(req, parts[1])
+      return cls._ExtractBasicUserPassword(parts[1])
 
     elif scheme == HTTP_DIGEST_AUTH.lower():
       # TODO: Implement digest authentication
@@ -193,39 +194,72 @@ class HttpServerRequestAuthentication(object):
       pass
 
     # Unsupported authentication scheme
-    return False
+    return None, None
 
-  def _CheckBasicAuthorization(self, req, in_data):
-    """Checks credentials sent for basic authentication.
+  @staticmethod
+  def _ExtractBasicUserPassword(in_data):
+    """Extracts user and password from the contents of an authorization header.
 
-    @type req: L{http.server._HttpServerRequest}
-    @param req: HTTP request context
     @type in_data: str
     @param in_data: Username and password encoded as Base64
-    @rtype: bool
-    @return: Whether user is allowed to execute request
+    @rtype: (str, str)
+    @return: A tuple containing user and password. One or both values might be
+             None if they are not presented
 
     """
     try:
       creds = base64.b64decode(in_data.encode("ascii")).decode("ascii")
     except (TypeError, binascii.Error, UnicodeError):
       logging.exception("Error when decoding Basic authentication credentials")
-      return False
+      raise http.HttpBadRequest(message=("Invalid basic authorization header"))
 
     if ":" not in creds:
-      return False
+      # We have just a username without password
+      return creds, None
 
-    (user, password) = creds.split(":", 1)
+    # return (user, password) tuple
+    return creds.split(":", 1)
 
-    return self.Authenticate(req, user, password)
-
-  def Authenticate(self, req, user, password):
-    """Checks the password for a user.
+  def Authenticate(self, req):
+    """Checks the credentiales.
 
     This function MUST be overridden by a subclass.
 
     """
     raise NotImplementedError()
+
+  @classmethod
+  def ExtractSchemePassword(cls, expected_password):
+    """Extracts a scheme and a password from the expected_password.
+
+    @type expected_password: str
+    @param expected_password: Username and password encoded as Base64
+    @rtype: (str, str)
+    @return: A tuple containing a scheme and a password. Both values will be
+             None when an invalid scheme or password encoded
+
+    """
+    if expected_password is None:
+      return None, None
+    # Backwards compatibility for old-style passwords without a scheme
+    if not expected_password.startswith("{"):
+      expected_password = cls._CLEARTEXT_SCHEME + expected_password
+
+    # Check again, just to be sure
+    if not expected_password.startswith("{"):
+      raise AssertionError("Invalid scheme")
+
+    scheme_end_idx = expected_password.find("}", 1)
+
+    # Ensure scheme has a length of at least one character
+    if scheme_end_idx <= 1:
+      logging.warning("Invalid scheme in password")
+      return None, None
+
+    scheme = expected_password[:scheme_end_idx + 1].upper()
+    password = expected_password[scheme_end_idx + 1:]
+
+    return scheme, password
 
   def VerifyBasicAuthPassword(self, req, username, password, expected):
     """Checks the password for basic authentication.
@@ -245,23 +279,10 @@ class HttpServerRequestAuthentication(object):
                      users file)
 
     """
-    # Backwards compatibility for old-style passwords without a scheme
-    if not expected.startswith("{"):
-      expected = self._CLEARTEXT_SCHEME + expected
 
-    # Check again, just to be sure
-    if not expected.startswith("{"):
-      raise AssertionError("Invalid scheme")
-
-    scheme_end_idx = expected.find("}", 1)
-
-    # Ensure scheme has a length of at least one character
-    if scheme_end_idx <= 1:
-      logging.warning("Invalid scheme in password for user '%s'", username)
+    scheme, expected_password = self.ExtractSchemePassword(expected)
+    if scheme is None or password is None:
       return False
-
-    scheme = expected[:scheme_end_idx + 1].upper()
-    expected_password = expected[scheme_end_idx + 1:]
 
     # Good old plain text password
     if scheme == self._CLEARTEXT_SCHEME:
@@ -283,4 +304,3 @@ class HttpServerRequestAuthentication(object):
                     scheme, username)
 
     return False
-

lib/rapi/auth/__init__.py
@@ -1,0 +1,52 @@
+#
+#
+
+# Copyright (C) 2015 Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""Module containing different authentificators that can be used by RAPI
+
+"""
+
+
+class RapiAuthenticator(object):
+  """Class providing authentication interface for RAPi requests.
+
+  """
+  def ValidateRequest(self, req, handler_access):
+    """Checks whether it's permitted to execute an rapi request.
+
+    Must be implemented in derived classes.
+
+    @type req: L{http.server._HttpServerRequest}
+    @param req: HTTP request context
+    @type handler_access: set of strings
+    @param handler_access: access rights required by the requested resourse
+    @rtype: bool
+    @return: Whether request execution is permitted
+
+    """
+    raise NotImplementedError()

commit 53e869fe138301ec8871d4c1e7dc76fa98465090

Fix RAPI auth bug introduced by f3b51166
This accidentally dropped the RemoteApiHandler.AuthenticationRequired
logic to check if a specific RAPI call handler required auth for
read/write access to the cluster config.

This wasn't noticed because some unit tests were changed to hardcode
auth to be required unconditionaly.

Signed-off-by: Brian Foley <bpfoley@google.com>
Reviewed-by: Viktor Bachraty <vbachraty@google.com>

lib/server/rapi.py
@@ -146,7 +146,12 @@ class RemoteApiHandler(http.auth.HttpServerRequestAuthentication,
     """Determine whether authentication is required.
 
     """
-    return self._reqauth
+    # Auth is required if
+    # a) Auth is forced to be enabled always.
+    # b) Auth isn't enforced, but the handler for that path/HTTP method
+    #    (GET, PUT etc) requires cluster config read/write access. An
+    #    example of this can be seen in rapi.rlib2.R_2_jobs_id_wait.GET_ACCESS.
+    return self._reqauth or bool(self._GetRequestContext(req).handler_access)
 
   def Authenticate(self, req):
     """Checks whether a user can access a resource.

test/py/ganeti.server.rapi_unittest.py
@@ -107,8 +107,7 @@ class TestRemoteApiHandler(unittest.TestCase):
     self.assertTrue(data["message"].startswith("Method PUT is unsupported"))
 
   def testPostInstancesNoAuth(self):
-    (code, _, _) = self._Test(http.HTTP_POST, "/2/instances", "", None,
-                              reqauth=True)
+    (code, _, _) = self._Test(http.HTTP_POST, "/2/instances", "", None)
     self.assertEqual(code, http.HttpUnauthorized.code)
 
   def testRequestWithUnsupportedMediaType(self):
@@ -137,8 +136,7 @@ class TestRemoteApiHandler(unittest.TestCase):
       "%s: %s" % (http.HTTP_AUTHORIZATION, "Unsupported scheme"),
       ])
 
-    (code, _, _) = self._Test(http.HTTP_POST, "/2/instances", headers, "",
-                              reqauth=True)
+    (code, _, _) = self._Test(http.HTTP_POST, "/2/instances", headers, "")
     self.assertEqual(code, http.HttpUnauthorized.code)
 
   def testIncompleteBasicAuth(self):
@@ -146,8 +144,7 @@ class TestRemoteApiHandler(unittest.TestCase):
       "%s: Basic" % http.HTTP_AUTHORIZATION,
       ])
 
-    (code, _, data) = self._Test(http.HTTP_POST, "/2/instances", headers, "",
-                                 reqauth=True)
+    (code, _, data) = self._Test(http.HTTP_POST, "/2/instances", headers, "")
     self.assertEqual(code, http.HttpBadRequest.code)
     self.assertEqual(data["message"],
                      "Basic authentication requires credentials")
@@ -159,8 +156,7 @@ class TestRemoteApiHandler(unittest.TestCase):
         "%s: Basic %s" % (http.HTTP_AUTHORIZATION, auth),
         ])
 
-      (code, _, data) = self._Test(http.HTTP_POST, "/2/instances", headers, "",
-                                   reqauth=True)
+      (code, _, data) = self._Test(http.HTTP_POST, "/2/instances", headers, "")
       self.assertEqual(code, http.HttpBadRequest.code)
 
   @staticmethod
@@ -202,7 +198,7 @@ class TestRemoteApiHandler(unittest.TestCase):
 
         for method in rapi.baserlib._SUPPORTED_METHODS:
           # No authorization
-          (code, _, _) = self._Test(method, path, "", "", reqauth=True)
+          (code, _, _) = self._Test(method, path, "", "")
 
           if method in (http.HTTP_DELETE, http.HTTP_POST):
             self.assertEqual(code, http.HttpNotImplemented.code)
@@ -212,22 +208,22 @@ class TestRemoteApiHandler(unittest.TestCase):
 
           # Incorrect user
           (code, _, _) = self._Test(method, path, header_fn(True), "",
-                                    user_fn=self._LookupWrongUser, reqauth=True)
+                                    user_fn=self._LookupWrongUser)
           self.assertEqual(code, http.HttpUnauthorized.code)
 
           # User has no write access, but the password is correct
           (code, _, _) = self._Test(method, path, header_fn(True), "",
-                                    user_fn=_LookupUserNoWrite, reqauth=True)
+                                    user_fn=_LookupUserNoWrite)
           self.assertEqual(code, http.HttpForbidden.code)
 
           # Wrong password and no write access
           (code, _, _) = self._Test(method, path, header_fn(False), "",
-                                    user_fn=_LookupUserNoWrite, reqauth=True)
+                                    user_fn=_LookupUserNoWrite)
           self.assertEqual(code, http.HttpUnauthorized.code)
 
           # Wrong password with write access
           (code, _, _) = self._Test(method, path, header_fn(False), "",
-                                    user_fn=_LookupUserWithWrite, reqauth=True)
+                                    user_fn=_LookupUserWithWrite)
           self.assertEqual(code, http.HttpUnauthorized.code)
 
           # Prepare request information
@@ -245,8 +241,7 @@ class TestRemoteApiHandler(unittest.TestCase):
           # User has write access, password is correct
           (code, _, data) = self._Test(method, reqpath, header_fn(True), body,
                                        user_fn=_LookupUserWithWrite,
-                                       luxi_client=_FakeLuxiClientForQuery,
-                                       reqauth=True)
+                                       luxi_client=_FakeLuxiClientForQuery)
           self.assertEqual(code, http.HTTP_OK)
           self.assertTrue(objects.QueryResponse.FromDict(data))
 