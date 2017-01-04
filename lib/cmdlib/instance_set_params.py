ganeti/ganeti

commit 77807d831976cf018ace04961820987a91c1d58a

Quell remaining pylint 1.6.4 warnings after merge
These should all be harmless modifications with no functional change.

Signed-off-by: Brian Foley <bpfoley@google.com>
Reviewed-by: Federico Pareschi <morg@google.com>

lib/cmdlib/instance_set_params.py
@@ -993,9 +993,10 @@ class LUInstanceSetParams(LogicalUnit):
                    if self.op.os_name and not self.op.force
                    else self.instance.os)
 
-    if (self.op.osparams or self.op.osparams_private or
-        self.op.clear_osparams or self.op.clear_osparams_private or
-        self.op.remove_osparams or self.op.remove_osparams_private):
+    if compat.any(
+        self.op.osparams, self.op.osparams_private,
+        self.op.clear_osparams, self.op.clear_osparams_private,
+        self.op.remove_osparams, self.op.remove_osparams_private):
       public_parms = self.op.osparams or {}
       private_parms = self.op.osparams_private or {}
       remove_osparams = self.op.remove_osparams or []

lib/backend.py
@@ -1683,8 +1683,8 @@ def AddNodeSshKeyBulk(node_list,
 
 
 # TODO: will be fixed with pending patch series.
-# pylint: disable=R0913
-def RemoveNodeSshKey(node_uuid, node_name,
+def RemoveNodeSshKey(node_uuid, # pylint: disable=R0913
+                     node_name,
                      master_candidate_uuids,
                      potential_master_candidates,
                      master_uuid=None,
@@ -2002,7 +2002,6 @@ def RemoveNodeSshKeyBulk(node_list,
       ssh.RemovePublicKey(node_uuid, key_file=pub_key_file)
 
   return result_msgs
-# pylint: enable=R0913
 
 
 def RemoveSshKeyFromPublicKeyFile(node_name,

lib/hypervisor/hv_kvm/__init__.py
@@ -907,6 +907,9 @@ class KVMHypervisor(hv_base.BaseHypervisor):
       target_process.set_cpu_affinity(range(psutil.cpu_count()))
     else:
       target_process.set_cpu_affinity(cpus)
+      # psutil 2.x deprecated (but still supports) get_children and the wrapper
+      # method sig doesn't show the keyword args, so quell the pylint warning.
+      # pylint: disable=E1123
       for p in target_process.get_children(recursive=True):
         p.set_cpu_affinity(cpus)
 

lib/rapi/testutils.py
@@ -362,7 +362,7 @@ class InputTestClient(object):
     password = utils.GenerateSecret()
 
     # pylint: disable=W0232
-    class SimpleAuthenticator():
+    class SimpleAuthenticator(object):
       # pylint: disable=R0201
       def ValidateRequest(self, req, _handler_access, _realm):
         """Called to verify user credentials given in HTTP request.

commit ba7013826ab681a5c2b5c1ae7d17dda9cc9d03f6

Fix incorrect 'any' function call
As introduced by commit 77807d83 and d388e30c6d
and was not caught during the code review process.

Signed-off-by: Federico Morg Pareschi <morg@google.com>
Reviewed-by: Brian Foley <bpfoley@google.com>

lib/cmdlib/instance_set_params.py
@@ -994,9 +994,9 @@ class LUInstanceSetParams(LogicalUnit):
                    else self.instance.os)
 
     if compat.any(
-        self.op.osparams, self.op.osparams_private,
-        self.op.clear_osparams, self.op.clear_osparams_private,
-        self.op.remove_osparams, self.op.remove_osparams_private):
+        [self.op.osparams, self.op.osparams_private,
+         self.op.clear_osparams, self.op.clear_osparams_private,
+         self.op.remove_osparams, self.op.remove_osparams_private]):
       public_parms = self.op.osparams or {}
       private_parms = self.op.osparams_private or {}
       remove_osparams = self.op.remove_osparams or []

tools/move-instance
@@ -943,9 +943,9 @@ def _CheckInstanceOptions(parser, options, instance_names):
       options.nics = cli.ParseNicOption(options.nics)
   else:
     # Moving more than one instance
-    if compat.any(options.dest_instance_name, options.dest_primary_node,
-                  options.dest_secondary_node, options.hvparams,
-                  options.beparams, options.osparams, options.nics):
+    if compat.any([options.dest_instance_name, options.dest_primary_node,
+                   options.dest_secondary_node, options.hvparams,
+                   options.beparams, options.osparams, options.nics]):
       parser.error("The options --dest-instance-name, --dest-primary-node,"
                    " --dest-secondary-node, --hypervisor-parameters,"
                    " --backend-parameters, --os-parameters and --net can"