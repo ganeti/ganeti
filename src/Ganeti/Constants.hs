c25da08369389593f11080821edcf0f8256bee3a
Robin H. Johnson <robbat2@gentoo.org>
2017-01-05 16:45:59 -0800

    kvm: use_guest_agent: QEMU Guest Agent support
    Implement the QEMU Guest Agent sockets, so that code/scripts on the
    hypervisors can communicate with guest operating systems easily.
    
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>
    Reviewed-by: Brian Foley <bpfoley@google.com>

--- src/Ganeti/Constants.hs
+++ src/Ganeti/Constants.hs
@@ -1814,6 +1814,9 @@ hvUsbMouse = "usb_mouse"
 hvUseBootloader :: String
 hvUseBootloader = "use_bootloader"
 
+hvUseGuestAgent :: String
+hvUseGuestAgent = "use_guest_agent"
+
 hvUseLocaltime :: String
 hvUseLocaltime = "use_localtime"
 
@@ -1948,6 +1951,7 @@ hvsParameterTypes = Map.fromList
   , (hvUsbDevices,                      VTypeString)
   , (hvUsbMouse,                        VTypeString)
   , (hvUseBootloader,                   VTypeBool)
+  , (hvUseGuestAgent,                   VTypeBool)
   , (hvUseLocaltime,                    VTypeBool)
   , (hvVga,                             VTypeString)
   , (hvVhostNet,                        VTypeBool)
@@ -4099,6 +4103,7 @@ hvcDefaults =
           , (hvMigrationBandwidth,              PyValueEx (32 :: Int))
           , (hvMigrationDowntime,               PyValueEx (30 :: Int))
           , (hvMigrationMode,                   PyValueEx htMigrationLive)
+          , (hvUseGuestAgent,                   PyValueEx False)
           , (hvUseLocaltime,                    PyValueEx False)
           , (hvDiskCache,                       PyValueEx htCacheDefault)
           , (hvSecurityModel,                   PyValueEx htSmNone)
