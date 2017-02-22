ganeti/ganeti

commit 7b1fd2e60e856ac2087413f664384d408ca48bf6

Cleanup: Remove unused/duplicate module/fn import
No functional change. Found by pylint's reimported warning.

Signed-off-by: Brian Foley <bpfoley@google.com>
Reviewed-by: Federico Pareschi <morg@google.com>

lib/server/rapi.py
@@ -61,7 +61,6 @@ from ganeti.rapi import connector
 from ganeti.rapi import baserlib
 
 import ganeti.http.auth   # pylint: disable=W0611
-import ganeti.http.server
 
 
 class RemoteApiRequestContext(object):

lib/cmdlib/cluster/__init__.py
@@ -67,7 +67,7 @@ from ganeti.cmdlib.common import ShareAll, RunPostHook, \
   CheckIpolicyVsDiskTemplates, CheckDiskAccessModeValidity, \
   CheckDiskAccessModeConsistency, GetClientCertDigest, \
   AddInstanceCommunicationNetworkOp, ConnectInstanceCommunicationNetworkOp, \
-  CheckImageValidity, CheckDiskAccessModeConsistency, EnsureKvmdOnNodes
+  CheckImageValidity, EnsureKvmdOnNodes
 
 import ganeti.masterd.instance
 

lib/cmdlib/instance_utils.py
@@ -44,7 +44,7 @@ from ganeti import pathutils
 from ganeti import utils
 from ganeti.cmdlib.common import AnnotateDiskParams, \
   ComputeIPolicyInstanceViolation, CheckDiskTemplateEnabled, \
-  CheckDiskTemplateEnabled, ComputeIPolicySpecViolation
+  ComputeIPolicySpecViolation
 
 
 #: Type description for changes as returned by L{ApplyContainerMods}'s

lib/metad.py
@@ -40,7 +40,7 @@ from ganeti import constants
 from ganeti import errors
 import ganeti.rpc.client as cl
 from ganeti.rpc.transport import Transport
-from ganeti.rpc import errors
+from ganeti.rpc.errors import TimeoutError
 
 
 # If the metadata daemon is disabled, there is no stub generated for it.
@@ -70,7 +70,7 @@ if constants.ENABLE_METAD:
         try:
           self._InitTransport()
           return
-        except errors.TimeoutError:
+        except TimeoutError:
           logging.debug("Timout trying to connect to MetaD")
           if try_no == retries - 1:
             raise

tools/move-instance
@@ -51,8 +51,6 @@ from ganeti import compat
 from ganeti import rapi
 from ganeti import errors
 
-import ganeti.rapi.client # pylint: disable=W0611
-import ganeti.rapi.client_utils
 from ganeti.rapi.client import UsesRapiClient
 
 

commit b26627e534e963334736fb7763648daac9c5258e

Fix missing import in RAPI server
This commit fixes a bug introduced in 7b1fd2e60 after pylint cleanup of
"unused" imports. We also need to suppress this warning as pylint cannot
determine that this import was actually used by the
'http.server.HttpServerHandler' symbol.

Signed-off-by: Yiannis Tsiouris <tsiou@grnet.gr>
Reviewed-by: Viktor Bachraty <vbachraty@google.com>

lib/server/rapi.py
@@ -61,6 +61,7 @@ from ganeti.rapi import connector
 from ganeti.rapi import baserlib
 
 import ganeti.http.auth   # pylint: disable=W0611
+import ganeti.http.server # pylint: disable=W0611
 
 
 class RemoteApiRequestContext(object):