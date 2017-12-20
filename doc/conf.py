85d335d0d617067f3a52179614b307aa468d9e80
Apollon Oikonomopoulos <apoikos@debian.org>
2017-12-20 11:52:07 +0200

    Fix documentation builds with Sphinx >= 1.4
    We are knowingly overriding sphinx's build-in manpage role, so we need
    to suppress sphinx's warnings; otherwise, when run with the `-W' switch,
    sphinx will error out.
    
    We also need to replace the unicode ellipses in doc/design-query2.rst
    with ascii periods, as sphinx amits another warning for not being able
    to properly highlight these lines as Python code.
    
    Signed-off-by: Apollon Oikonomopoulos <apoikos@debian.org>

--- doc/conf.py
+++ doc/conf.py
@@ -229,3 +229,8 @@ latex_documents = [
 
 # If false, no module index is generated.
 latex_use_modindex = False
+
+# We override the manpage role and sphinx issues a warning, which is treated as
+# error. Suppress role_add warnings to avoid FTBFS.
+
+suppress_warnings = ["app.add_role"]
