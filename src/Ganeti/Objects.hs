18dc6f8563e4ed42fdcbf86b1d2f2443a79f807c
Apollon Oikonomopoulos <apoikos@debian.org>
2018-11-02 12:14:57 +0200

    Make all instances of Monoid, instances of Semigroup
    This is a compatibility fix for base-4.11/GHC 8.4. In base-4.11,
    Semigroup is a superclass of Monoid and Semigroup itself is exported in
    Prelude.
    
    Make all our Monoid instances also instances of Semigroup and define
    mappend in terms of (<>).
    
    Note that to keep compatibility with base-4.9 and avoid duplicate import
    warnings, we do a qualified import of Data.Semigroup as Sem. This can be
    removed once we drop compatibility for anything earlier than base-4.11.
    
    This commit fixes part of #1312.
    
    Signed-off-by: Apollon Oikonomopoulos <apoikos@debian.org>
    (cherry picked from commit 68e87455f9c6d42533680a2aa36ffcb75d90fee5)

--- src/Ganeti/Objects.hs
+++ src/Ganeti/Objects.hs
@@ -115,6 +115,7 @@ import qualified Data.Map as Map
 import Data.Monoid
 import Data.Ord (comparing)
 import Data.Ratio (numerator, denominator)
+import qualified Data.Semigroup as Sem
 import Data.Tuple (swap)
 import Data.Word
 import Text.JSON (showJSON, readJSON, JSON, JSValue(..), fromJSString,
@@ -286,12 +287,15 @@ $(buildObject "DataCollectorConfig" "dataCollector" [
   ])
 
 -- | Central default values of the data collector config.
+instance Sem.Semigroup DataCollectorConfig where
+  _ <> a = a
+
 instance Monoid DataCollectorConfig where
   mempty = DataCollectorConfig
     { dataCollectorActive = True
     , dataCollectorInterval = 10^(6::Integer) * fromIntegral C.mondTimeInterval
     }
-  mappend _ a = a
+  mappend = (Sem.<>)
 
 -- * IPolicy definitions
 
