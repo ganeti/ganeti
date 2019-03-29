ganeti/ganeti

commit cd2afc33284f32bad3b31cef6d8d6e5dc01d6e26

Convert TagSet to a "proper" type
Type aliases have multiple problems, one of which hits us in the form
of (upstream/default) Arbitrary instances. For TagSet, that means the
arbitrary instance will be a set of random strings, which doesn't give
as good a coverage as our desired tag formats.

Changing this to its own proper type is much simpler than I thought,
since the JSON instance is the heavy-usage path; there is only one
other case where we need to adjust to the new type.

Tests fail in the same way as before this patch ('jobFiltering'
property fails coverage).

Signed-off-by: Iustin Pop <iustin@k1024.org>

src/Ganeti/THH/Field.hs
@@ -43,7 +43,7 @@ module Ganeti.THH.Field
   , timeStampFields
   , uuidFields
   , serialFields
-  , TagSet
+  , TagSet(..)
   , emptyTagSet
   , tagsFields
   , fileModeAsIntField
@@ -122,12 +122,17 @@ serialFields =
 uuidFields :: [Field]
 uuidFields = [ presentInForthcoming $ simpleField "uuid" [t| BS.ByteString |] ]
 
--- | Tag set type alias.
-type TagSet = Set.Set String
+-- | Tag set type.
+newtype TagSet = TagSet { unTagSet :: Set.Set String }
+  deriving (Eq, Show)
+
+instance JSON.JSON TagSet where
+  showJSON = JSON.showJSON . unTagSet
+  readJSON = (TagSet <$>) . JSON.readJSON
 
 -- | Empty tag set value.
 emptyTagSet :: TagSet
-emptyTagSet = Set.empty
+emptyTagSet = TagSet Set.empty
 
 -- | Tag field description.
 tagsFields :: [Field]

src/Ganeti/Objects.hs
@@ -84,7 +84,7 @@ module Ganeti.Objects
   , SerialNoObject(..) -- re-exported from Types
   , TagsObject(..) -- re-exported from Types
   , DictObject(..) -- re-exported from THH
-  , TagSet -- re-exported from THH
+  , TagSet(..) -- re-exported from THH
   , emptyTagSet -- re-exported from THH
   , Network(..)
   , AddressPool(..)

src/Ganeti/WConfd/Ssconf.hs
@@ -88,7 +88,7 @@ mkSSConfHvparams cluster = map (id &&& hvparams) [minBound..maxBound]
 mkSSConf :: ConfigData -> SSConf
 mkSSConf cdata = SSConf . M.fromList $
     [ (SSClusterName, return $ clusterClusterName cluster)
-    , (SSClusterTags, toList $ tagsOf cluster)
+    , (SSClusterTags, toList . unTagSet $ tagsOf cluster)
     , (SSFileStorageDir, return $ clusterFileStorageDir cluster)
     , (SSSharedFileStorageDir, return $ clusterSharedFileStorageDir cluster)
     , (SSGlusterStorageDir, return $ clusterGlusterStorageDir cluster)

test/hs/Test/Ganeti/Objects.hs
@@ -102,7 +102,7 @@ instance Arbitrary Node where
               <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
               <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
               <*> fmap UTF8.fromString genUUID <*> arbitrary
-              <*> (Set.fromList <$> genTags)
+              <*> arbitrary
 
 $(genArbitrary ''BlockDriver)
 
@@ -193,7 +193,7 @@ instance Arbitrary ForthcomingInstanceData where
       -- serial
       <*> arbitrary
       -- tags
-      <*> (Set.fromList <$> genTags)
+      <*> arbitrary
 
 instance Arbitrary RealInstanceData where
   arbitrary =
@@ -234,7 +234,7 @@ instance Arbitrary RealInstanceData where
       -- serial
       <*> arbitrary
       -- tags
-      <*> (Set.fromList <$> genTags)
+      <*> arbitrary
 
 instance Arbitrary Instance where
   arbitrary = frequency [ (1, ForthcomingInstance <$> arbitrary)
@@ -648,7 +648,7 @@ genNodeGroup = do
   mtime <- arbitrary
   uuid <- genFQDN `suchThat` (/= name)
   serial <- arbitrary
-  tags <- Set.fromList <$> genTags
+  tags <- arbitrary
   let group = NodeGroup name members ndparams alloc_policy ipolicy diskparams
               net_map hv_state disk_state ctime mtime (UTF8.fromString uuid)
               serial tags

test/hs/Test/Ganeti/TestCommon.hs
@@ -118,19 +118,19 @@ import Numeric
 
 import qualified Ganeti.BasicTypes as BasicTypes
 import Ganeti.JSON (ArrayObject(..))
-import Ganeti.Objects (TagSet)
+import Ganeti.Objects (TagSet(..))
 import Ganeti.Types
 import Ganeti.Utils.Monad (unfoldrM)
 
 -- * Arbitrary orphan instances
 
+instance Arbitrary TagSet where
+  arbitrary = (TagSet . Set.fromList) <$> genTags
+
 #if !MIN_VERSION_QuickCheck(2,8,0)
 instance (Ord k, Arbitrary k, Arbitrary a) => Arbitrary (M.Map k a) where
   arbitrary = M.fromList <$> arbitrary
   shrink m = M.fromList <$> shrink (M.toList m)
-
-instance Arbitrary TagSet where
-  arbitrary = Set.fromList <$> genTags
 #endif
 
 -- * Constants

commit fe62a283ca6452b7103197ec6c18a148d5408482

Ganeti.THH.Field: import <$> explicitly
Commit cd2afc33 ("Convert TagSet to a "proper" type") introduced a use
of <$> in Ganeti.THH.Field which assumes <$> to be available in Prelude.
However this is not the case for GHC 7.6 (which we still need to support
for stable-2.16), so import <$> explicitly.

Fixes: cd2afc33 ("Convert TagSet to a "proper" type")
Signed-off-by: Apollon Oikonomopoulos <apoikos@dmesg.gr>

src/Ganeti/THH/Field.hs
@@ -50,6 +50,7 @@ module Ganeti.THH.Field
   , processIdField
   ) where
 
+import Control.Applicative ((<$>))
 import Control.Monad
 import qualified Data.ByteString as BS
 import qualified Data.Set as Set