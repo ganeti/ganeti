12a3df1e21d7d064baa9df694e8c925aef92075a
Apollon Oikonomopoulos <apoikos@dmesg.gr>
2019-04-09 12:39:42 +0300

    ht: use unicode_escape instead of string_escape
    string_escape is no more in Python 3, use unicode_escape instead.
    
    Signed-off-by: Apollon Oikonomopoulos <apoikos@dmesg.gr>

--- lib/ht.py
+++ lib/ht.py
@@ -333,7 +333,7 @@ def TRegex(pobj):
 
   """
   desc = WithDesc("String matching regex \"%s\"" %
-                  pobj.pattern.encode("string_escape"))
+                  pobj.pattern.encode("unicode_escape"))
 
   return desc(TAnd(TString, pobj.match))
 
