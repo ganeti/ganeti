c0902a5f2c860ce76538cd8900e2b8848139b1d1
Apollon Oikonomopoulos <apoikos@dmesg.gr>
2019-04-25 10:57:47 +0300

    daemon: always pass bytes to sockets
    The socket API now expects bytes, so adjust accordingly.
    
    Signed-off-by: Apollon Oikonomopoulos <apoikos@dmesg.gr>

--- lib/daemon.py
+++ lib/daemon.py
@@ -339,6 +339,8 @@ class AsyncUDPSocket(GanetiBaseAsyncoreDispatcher):
     """Enqueue a datagram to be sent when possible
 
     """
+    if isinstance(payload, str):
+      payload = payload.encode("utf-8")
     if len(payload) > constants.MAX_UDP_DATA_SIZE:
       raise errors.UdpDataSizeError("Packet too big: %s > %s" % (len(payload),
                                     constants.MAX_UDP_DATA_SIZE))
@@ -412,7 +414,7 @@ class AsyncAwaker(GanetiBaseAsyncoreDispatcher):
     # sending more than one wakeup token, which doesn't harm at all.
     if self.need_signal:
       self.need_signal = False
-      self.out_socket.send(chr(0))
+      self.out_socket.send(b"\x00")
 
 
 class _ShutdownCheck(object):
