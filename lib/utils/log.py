6a8367de0fa24355c499ab2e0aaa575bbedaddb9
Apollon Oikonomopoulos <apoikos@dmesg.gr>
2019-06-19 18:23:23 +0200

    log: do not open console in append mode
    Python 3 tries to seek.

--- lib/utils/log.py
+++ lib/utils/log.py
@@ -259,7 +259,7 @@ def SetupLogging(logfile, program, debug=0, stderr_logging=False,
     # exception since otherwise we could run but without any logs at all
     try:
       if console_logging:
-        logfile_handler = _LogHandler(open(constants.DEV_CONSOLE, "a"),
+        logfile_handler = _LogHandler(open(constants.DEV_CONSOLE, "w"),
                                       logfile)
       else:
         logfile_handler = _ReopenableLogHandler(logfile)
