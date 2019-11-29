6d9c9e1c62d9e257f8f78257d4d4b400a370d821
Apollon Oikonomopoulos <apoikos@dmesg.gr>
2019-11-29 18:16:19 +0200

    Port Ganeti.Hash to cryptonite
    There's a bug exposed by the combination of Crypto and GHC 8.6 that
    results in an endless loop[1], caused by GHC 8.6 changing accumArray
    from lazy to strict. The endless loop can be reproduced as follows:
    
     Prelude> :m +Data.HMAC
     Prelude Data.HMAC> hmac_sha1 [] []
    
    Unfortunately Crypto was last updated in 2012 and seems to be largely
    unmaintained, so at this point it's probably a good idea to switch our
    HMAC implementation over to cryptonite, which is the de facto standard
    for crypto in Haskell these days.
    
    [1] https://gitlab.haskell.org/ghc/ghc/issues/16406
    
    Signed-off-by: Apollon Oikonomopoulos <apoikos@dmesg.gr>

--- src/Ganeti/Hash.hs
+++ src/Ganeti/Hash.hs
@@ -39,29 +39,21 @@ module Ganeti.Hash
   ) where
 
 import qualified Data.ByteString as B
+import qualified Data.ByteString.UTF8 as BU
+import Crypto.Hash.Algorithms
+import Crypto.MAC.HMAC
 import Data.Char
-import Data.HMAC (hmac_sha1)
-import qualified Data.Text as T
-import Data.Text.Encoding (encodeUtf8)
 import Data.Word
-import Text.Printf (printf)
 
 -- | Type alias for the hash key. This depends on the library being
 -- used.
 type HashKey = [Word8]
 
--- | Converts a string to a list of bytes.
-stringToWord8 :: String -> HashKey
-stringToWord8 = B.unpack . encodeUtf8 . T.pack
-
--- | Converts a list of bytes to a string.
-word8ToString :: HashKey -> String
-word8ToString = concatMap (printf "%02x")
-
 -- | Computes the HMAC for a given key/test and salt.
 computeMac :: HashKey -> Maybe String -> String -> String
 computeMac key salt text =
-  word8ToString . hmac_sha1 key . stringToWord8 $ maybe text (++ text) salt
+  let hashable = maybe text (++ text) salt
+  in show . hmacGetDigest $ (hmac (B.pack key) (BU.fromString hashable) :: HMAC SHA1)
 
 -- | Verifies the HMAC for a given message.
 verifyMac :: HashKey -> Maybe String -> String -> String -> Bool
