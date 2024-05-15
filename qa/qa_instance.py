a2301198a583a1a05ca6ab7db5a14ae07bf69bed
Sascha Lucas <sascha_lucas@web.de>
2024-05-16 00:34:11 +0200

    a blind guess to fix QA and DNS checking
    The idea is, that before the name-check was implicitly enabled. Now it
    must be explicitly enabled via extra CLI option.
    
    Signed-off-by: Sascha Lucas <sascha_lucas@web.de>

--- qa/qa_instance.py
+++ qa/qa_instance.py
@@ -465,7 +465,7 @@ def TestInstanceRenameAndBack(rename_source, rename_target):
   # first do a rename to a different actual name, expecting it to fail
   qa_utils.AddToEtcHosts(["meeeeh-not-exists", rename_target])
   try:
-    AssertCommand(["gnt-instance", "rename", rename_source, rename_target],
+    AssertCommand(["gnt-instance", "rename", "--name-check", rename_source, rename_target],
                   fail=True)
     CheckSsconfInstanceList(rename_source)
   finally:
@@ -489,7 +489,7 @@ def TestInstanceRenameAndBack(rename_source, rename_target):
     tags_cmd = "false"
 
   # and now rename instance to rename_target...
-  AssertCommand(["gnt-instance", "rename", rename_source, rename_target])
+  AssertCommand(["gnt-instance", "rename", "--name-check", rename_source, rename_target])
   CheckSsconfInstanceList(rename_target)
   qa_utils.RunInstanceCheck(rename_source, False)
   qa_utils.RunInstanceCheck(rename_target, False)
@@ -503,7 +503,7 @@ def TestInstanceRenameAndBack(rename_source, rename_target):
       AssertCommand(tags_cmd + rename_target, node=node, fail=False)
 
   # and back
-  AssertCommand(["gnt-instance", "rename", rename_target, rename_source])
+  AssertCommand(["gnt-instance", "rename", "--name-check", rename_target, rename_source])
   CheckSsconfInstanceList(rename_source)
   qa_utils.RunInstanceCheck(rename_target, False)
 
