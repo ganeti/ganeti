099cfccf73b2ccb2b51c3d8daeb01ada2f35d21a
thielema <git@henning-thielemann.de>
2024-08-13 13:09:12 +0200

    Test.Ganeti.Utils: correct instance Arbitrary ClockTime
    There was one digit missing to 10^12-1.

--- test/hs/Test/Ganeti/Utils.hs
+++ test/hs/Test/Ganeti/Utils.hs
@@ -291,7 +291,8 @@ prop_clockTimeToString ts pico =
   clockTimeToString (TOD ts pico) ==? show ts
 
 instance Arbitrary ClockTime where
-  arbitrary = TOD <$> choose (946681200, 2082754800) <*> choose (0, 99999999999)
+  arbitrary =
+    TOD <$> choose (946681200, 2082754800) <*> choose (0, 999999999999)
 
 -- | Verify our work-around for ghc bug #2519. Taking `diffClockTimes` form
 -- `System.Time`, this test fails with an exception.
