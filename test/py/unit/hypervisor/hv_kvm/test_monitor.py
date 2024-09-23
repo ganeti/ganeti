85337b216af0ff3f82c7c9904d4ca5f00191ea9a
codefritzel <pascal-pf@hotmail.de>
2024-09-23 15:22:04 +0200

    add chunked & multiple msg; qmp timeout Union
    This adds chunked & multiple msg tests for the socket
    recv_qmp() method and adds Union type hints for the
    qmp timeout to use int and float datatypes.
    
    Signed-off-by: codefritzel <pascal-pf@hotmail.de>

--- test/py/unit/hypervisor/hv_kvm/test_monitor.py
+++ test/py/unit/hypervisor/hv_kvm/test_monitor.py
@@ -120,6 +120,8 @@ class FakeQmpSocket(threading.Thread):
   def __init__(self, socket_path):
     threading.Thread.__init__(self)
     self._is_running = True
+    self._is_simulate = True
+    self._conn = None
     self.socket_path = socket_path
 
     self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -129,6 +131,7 @@ class FakeQmpSocket(threading.Thread):
   def run(self):
 
     conn, _ = self.socket.accept()
+    self._conn = conn
 
     # send the banner first
     conn.send(encode_data(QMP_BANNER_DATA))
@@ -142,17 +145,23 @@ class FakeQmpSocket(threading.Thread):
     conn.send(encode_data(get_supported_commands()))
 
     while self._is_running:
-      data = conn.recv(4096)
-      if data != b'':
-        msg = QmpMessage.build_from_json_string(data.decode('utf-8'))
-        cmd = msg['execute']
+      while self._is_simulate:
+        data = conn.recv(4096)
+        if data != b'':
+          msg = QmpMessage.build_from_json_string(data.decode('utf-8'))
+          cmd = msg['execute']
 
-        # search for a function in this file with name simulate_<command>
-        if cmd in FAKE_QMP_COMMANDS.keys():
-          func = FAKE_QMP_COMMANDS[cmd]
-          func(conn, msg.data.get('arguments', {}))
+          # check if the function exists with simulate_qmp decorator
+          if cmd in FAKE_QMP_COMMANDS.keys():
+            func = FAKE_QMP_COMMANDS[cmd]
+            func(conn, msg.data.get('arguments', {}))
 
     conn.close()
+
+  def send(self, data: bytes):
+    self._is_simulate = False
+    self._conn.send(data)
+    self._is_simulate = True
 
   def stop(self):
     self._is_running = False
@@ -162,18 +171,27 @@ class FakeQmpSocket(threading.Thread):
 class TestQmpConnection:
 
   @pytest.fixture
-  def fake_qmp(self):
-    socket_path = tempfile.NamedTemporaryFile().name
-    #  os.remove(socket_path)
+  def fake_socket_path(self) -> str:
+    return tempfile.NamedTemporaryFile().name
 
-    fake_qmp_socket = FakeQmpSocket(socket_path)
+  @pytest.fixture
+  def fake_qmp_socket(self, fake_socket_path):
+    fake_qmp_socket = FakeQmpSocket(fake_socket_path)
     fake_qmp_socket.daemon = True
     fake_qmp_socket.start()
-    qmp = QmpConnection(socket_path)
+
+    yield fake_qmp_socket
+
+    fake_qmp_socket.stop()
+
+  @pytest.fixture
+  def fake_qmp(self, fake_qmp_socket, fake_socket_path):
+    qmp = QmpConnection(fake_socket_path)
     qmp.timeout = 1
     yield qmp
 
-    fake_qmp_socket.stop()
+    if qmp.is_connected():
+      qmp.close()
 
   def test_connect(self, fake_qmp: QmpConnection):
     fake_qmp.connect()
@@ -189,6 +207,26 @@ class TestQmpConnection:
     assert (fake_qmp.supported_commands ==
             frozenset(
               item["name"] for item in get_supported_commands()['return']))
+
+  def test_recv_qmp(self, fake_qmp: QmpConnection, fake_qmp_socket):
+    fake_qmp.connect()
+
+    # get one qmp message with multiple socket send pieces
+    send_pieces = ['{"ret', 'ur', 'n": {}}\r\n']
+    for piece in send_pieces:
+      fake_qmp_socket.send(piece.encode('utf-8'))
+    qmp = fake_qmp.recv_qmp()
+    assert qmp == QmpMessage.build_from_json_string("".join(send_pieces))
+
+    # send two messages in one send and parse two
+    two_msgs = ['{"return": [{"name": "quit"}, {"name": "eject"}]}\r\n',
+                '{"return": {"running": true, "singlestep": false}}\r\n']
+    # combine the two strings into one
+    fake_qmp_socket.send("".join(two_msgs).encode('utf-8'))
+    qmp_msg0 = fake_qmp.recv_qmp()
+    qmp_msg1 = fake_qmp.recv_qmp()
+    assert (qmp_msg0 == QmpMessage.build_from_json_string(two_msgs[0]) and
+            qmp_msg1 == QmpMessage.build_from_json_string(two_msgs[1]))
 
   def test_execute_qmp(self, fake_qmp: QmpConnection):
     arguments = {
